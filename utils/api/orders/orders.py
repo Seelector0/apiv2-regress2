b475f89aa5e2cca220206f04178c764c48e0bfd0
fox140 <andrey.malcev@pimpay.ru>
2023-01-19 20:56:59 +0300

    Правки json файлов

--- utils/api/orders/orders.py
+++ utils/api/orders/orders.py
@@ -7,63 +7,71 @@ class ApiOrder:
 
     @staticmethod
     def json_order(warehouse_id: str, shop_id: str, payment_type: str, type_ds: str, service: str, tariff: str,
-                   price: float, declared_value: float, delivery_point_code=None):
+                   price: float, declared_value: float, delivery_point_code=None, weight: float = randint(3, 10),
+                   length: float = randint(15, 50), width: float = randint(15, 50), height: float = randint(15, 50)):
         """Json создания заказа"""
-        weight = f"{randint(3, 10)}"
-        json_create_order = json.dumps({
-            "warehouse": {
-            "id": f"{warehouse_id}",
-            },
-            "shop": {
-            "id": f"{shop_id}",
-            "number": f"{randrange(100000, 999999)}"
-            },
-            "payment": {
-                "type": payment_type,
-                "declaredValue": declared_value,
-                "deliverySum": 100.24,
-            },
-            "dimension": {
-                "length": f"{randint(15, 50)}",
-                "width": f"{randint(15, 50)}",
-                "height": f"{randint(15, 50)}"
-            },
-            "weight": weight,
-            "delivery": {
-                "type": type_ds,
-                "service": service,
-                "tariff": tariff,
-                "date": "",
-                "time": {
-                    "from": "",
-                    "to": ""
+        json_create_order = json.dumps(
+            {
+                "warehouse": {
+                    "id": f"{warehouse_id}",
                 },
-                "deliveryPointCode": delivery_point_code
-            },
-            "recipient": {
-                "fullName": "Филипенко Юрий Павлович",
-                "phoneNumber": f"909{randrange(1000000, 9999999)}",
-                "address": {
-                    "raw": "115035, г Москва, р-н Замоскворечье, Садовническая наб, д 14 стр 2"
-                }
-            },
-            "comment": "",
-            "places": [
-                {
-            "items": [
-                {
-                    "article": f"ART{randrange(1000000, 9999999)}",
-                    "name": "111",
-                    "price": price,
-                    "count": 1,
-                    "weight": weight,
-                    "vat": "NO_VAT"
-                }
-            ]
-                }
-            ]
-        })
+                "shop": {
+                    "id": f"{shop_id}",
+                    "number": f"{randrange(100000, 999999)}"
+                },
+                "payment": {
+                    "type": payment_type,
+                    "declaredValue": declared_value,
+                    "deliverySum": 100.24,
+                },
+                "dimension": {
+                    "length": length,
+                    "width": width,
+                    "height": height
+                },
+                "weight": weight,
+                "delivery": {
+                    "type": type_ds,
+                    "service": service,
+                    "tariff": tariff,
+                    "date": "",
+                    "time": {
+                        "from": "",
+                        "to": ""
+                    },
+                    "deliveryPointCode": delivery_point_code
+                },
+                "recipient": {
+                    "fullName": "Филипенко Юрий Павлович",
+                    "phoneNumber": f"+7909{randrange(1000000, 9999999)}",
+                    "address": {
+                        "raw": "115035, г Москва, р-н Замоскворечье, Садовническая наб, д 14 стр 2"
+                    }
+                },
+                "comment": "",
+                "places": [
+                    {
+                        "items": [
+                            {
+                                "article": f"ART{randrange(1000000, 9999999)}",
+                                "name": "111",
+                                "price": price,
+                                "count": 1,
+                                "weight": weight,
+                                "vat": "NO_VAT"
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
         return json_create_order
+
+    @staticmethod
+    def search_order(params, headers):
+        """Метод поиска по заказам"""
+        search_order = HttpMethods.get(link="/orders/search", params=params, headers=headers)
+        return search_order
 
     @staticmethod
     def create_order(warehouse_id, shop_id, payment_type, type_ds, service, tariff, price, declared_value, headers,
@@ -72,47 +80,59 @@ class ApiOrder:
         order = ApiOrder.json_order(warehouse_id=warehouse_id, shop_id=shop_id, payment_type=payment_type,
                                     type_ds=type_ds, service=service, tariff=tariff, price=price,
                                     declared_value=declared_value, delivery_point_code=delivery_point_code)
-        create_order = HttpMethods.post(link="/orders", data=order, headers=headers)
-        return create_order
+        result_create_order = HttpMethods.post(link="/orders", data=order, headers=headers)
+        return result_create_order
 
     @staticmethod
     def get_orders_list(headers: dict):
         """Метод возвращает список заказов"""
-        get_order_list = HttpMethods.get(link="/orders", headers=headers)
-        return get_order_list
+        result_get_order_list = HttpMethods.get(link="/orders", headers=headers)
+        return result_get_order_list
 
     @staticmethod
     def get_order_by_id(headers: dict, order_id: str):
         """Метод получения информации о заказе по его id"""
-        get_odre_by_id = HttpMethods.get(link=f"/orders/{order_id}", headers=headers)
-        return get_odre_by_id
+        result_get_odre_by_id = HttpMethods.get(link=f"/orders/{order_id}", headers=headers)
+        return result_get_odre_by_id
 
     @staticmethod
     def update_order(order_id: str, headers: dict):
         """Метод обновления заказ"""
+        # Todo надо будет дописать метод
         pass
 
     @staticmethod
     def update_fields_order(order_id: str, headers: dict):
         """Метод обновления поля в заказе"""
+        # Todo надо будет дописать метод
         pass
 
     @staticmethod
     def delete_order(order_id: str, headers: dict):
-        delete_order = HttpMethods.delete(link=f"/orders/{order_id}", headers=headers)
-        return delete_order
+        """Метод удаления заказа"""
+        result_delete_order = HttpMethods.delete(link=f"/orders/{order_id}", headers=headers)
+        return result_delete_order
 
     @staticmethod
     def get_order_patches(order_id: str, headers: dict):
-        get_order_patches = HttpMethods.get(link=f"/orders/{order_id}/patches", headers=headers)
-        return get_order_patches
+        """Получение PATCH изменений по заказу"""
+        result_get_order_patches = HttpMethods.get(link=f"/orders/{order_id}/patches", headers=headers)
+        return result_get_order_patches
 
     @staticmethod
     def get_order_statuses(order_id: str, headers: dict):
-        get_order_statuses = HttpMethods.get(link=f"/orders/{order_id}/statuses", headers=headers)
-        return get_order_statuses
+        """Получение информации об истории изменения статусов заказа"""
+        result_get_order_statuses = HttpMethods.get(link=f"/orders/{order_id}/statuses", headers=headers)
+        return result_get_order_statuses
 
     @staticmethod
     def get_order_details(order_id: str, headers: dict):
-        get_order_details = HttpMethods.get(link=f"/orders/{order_id}/details", headers=headers)
-        return get_order_details
+        """Получение подробной информации о заказе"""
+        result_get_order_details = HttpMethods.get(link=f"/orders/{order_id}/details", headers=headers)
+        return result_get_order_details
+
+    @staticmethod
+    def get_label(order_id: str, headers: dict):
+        """Метод получения этикетки заказа"""
+        result_get_label = HttpMethods.get(link=f"orders/{order_id}/label", headers=headers)
+        return result_get_label
