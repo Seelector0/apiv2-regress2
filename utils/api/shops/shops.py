d63da0d83129942d15275d6330318d40e40b673c
fox140 <andrey.malcev@pimpay.ru>
2023-01-23 12:51:27 +0300

    Сделал методы не статичными для работы через Application

--- utils/api/shops/shops.py
+++ utils/api/shops/shops.py
@@ -1,69 +1,59 @@
-from utils.http_methods import HttpMethods
-from model.shops import Shop
 from random import randrange
 import json
 
 
 class ApiShop:
 
-    @staticmethod
-    def json_shop(shop_name: str):
-        shop = json.dumps(
+    def __init__(self, app):
+        self.app = app
+
+    def json_shop(self, shop_name: str = f"INT{randrange(100000, 999999)}",
+                  url_shop: str = f"integration-shop{randrange(1000, 9999)}.ru",
+                  phone: str = f"7916{randrange(1000000, 9999999)}",
+                  contact_person: str = "Иванов Иван Иванович"):
+        """Json создания магазина"""
+        json_create_shop = json.dumps(
             {
                 "name": shop_name,
-                "uri": f"integration-shop{randrange(1000, 9999)}.ru",
-                "phone": f"7916{randrange(1000000, 9999999)}",
-                "sender": "Иванов Иван Иванович"
+                "uri": url_shop,
+                "phone": phone,
+                "sender": contact_person
             }
         )
-        return shop
+        return json_create_shop
 
-    @staticmethod
-    def create_shop(headers: dict, shop_name: str):
+    def create_shop(self, headers: dict):
         """Создание магазина"""
-        shop = ApiShop.json_shop(shop_name=shop_name)
-        result_post_shop = HttpMethods.post(link="/customer/shops", data=shop, headers=headers)
+        new_shop = self.json_shop()
+        result_post_shop = self.app.http_method.post(link="/customer/shops", data=new_shop, headers=headers)
         return result_post_shop
 
-    @staticmethod
-    def get_shop(headers: dict):
+    def get_shop(self, headers: dict):
         """Метод получения списка магазинов"""
-        result_get_shop = HttpMethods.get(link="/customer/shops", headers=headers)
+        result_get_shop = self.app.http_method.get(link="/customer/shops", headers=headers)
         return result_get_shop
 
-    @staticmethod
-    def get_shop_by_id(headers: dict, shop_id: str):
+    def get_shop_by_id(self , headers: dict, shop_id: str):
         """Метод получения магазина по его id"""
-        result_get_shop_by_id = HttpMethods.get(link=f"/customer/shops/{shop_id}", headers=headers)
+        result_get_shop_by_id = self.app.http_method.get(link=f"/customer/shops/{shop_id}", headers=headers)
         return result_get_shop_by_id
 
-    @staticmethod
-    def put_shop(headers: dict, shop_id: str, shop_name: str):
+    def put_shop(self, headers: dict, shop_id: str, json_shop):
         """Метод редактирования магазина"""
-        shop = ApiShop.json_shop(shop_name=shop_name)
-        result_put = HttpMethods.put(link=f"/customer/shops/{shop_id}", data=shop, headers=headers)
+        shop = self.json_shop(json_shop)
+        result_put = self.app.http_method.put(link=f"/customer/shops/{shop_id}", data=shop, headers=headers)
         return result_put
 
-    @staticmethod
-    def patch_shop(headers: dict, shop_id: str, value: bool):
-        """Метод делает магазин не активным"""
-        body = json.dumps([
-            {
-                "op": "replace",
-                "path": "visibility",
-                "value": value
-            }
-        ])
-        result_patch = HttpMethods.patch(link=f"/customer/shops/{shop_id}", data=body, headers=headers)
+    def patch_shop(self, headers: dict, shop_id: str, value: bool):
+        """Метод делает магазин видимым или не видимым для ЛК"""
+        body = json.dumps(
+            [
+                {
+                    "op": "replace",
+                    "path": "visibility",
+                    "value": value
+                }
+            ]
+        )
+        result_patch = self.app.http_method.patch(link=f"/customer/shops/{shop_id}", data=body, headers=headers)
         return result_patch
-
-    @staticmethod
-    def get_shops_list(headers: dict):
-        """Функция собирает список магазинов"""
-        shop_list = []
-        shops = HttpMethods.get(link="/customer/shops", headers=headers)
-        for element in shops.json():
-            shop_id = element["id"]
-            name = element["name"]
-            shop_list.append(Shop(shop_id=shop_id, shop_name=name))
-        return shop_list
