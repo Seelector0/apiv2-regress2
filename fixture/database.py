86a04b222aaf10fffe20b7234b2df741ad8f94d9
fox140 <andrey.malcev@pimpay.ru>
2023-01-23 12:43:53 +0300

    Убрал метод подключения к БД в родительский класс

--- fixture/database.py
+++ fixture/database.py
@@ -1,13 +1,17 @@
-"""Метод подключения к базе данных."""
+import psycopg2
+import os
 
 
 class DataBase:
 
     def __init__(self, host, database, user, password):
+        """Метод подключения к базе данных"""
         self.host = host
         self.database = database
         self.user = user
         self.password = password
+        self.connection = psycopg2.connect(host=host, database=database, user=user, password=password)
+        self.user_id = os.getenv("USER_ID")
 
     def connection_close(self):
         """Выход из базы данных"""
