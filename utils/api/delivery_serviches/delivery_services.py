a98d3612d7a8d750b4752b2e9c05c97d75372405
fox140 <andrey.malcev@pimpay.ru>
2023-01-25 00:31:16 +0300

    Убрал staticmethod

--- utils/api/delivery_serviches/delivery_services.py
+++ utils/api/delivery_serviches/delivery_services.py
@@ -9,9 +9,12 @@ load_dotenv(find_dotenv())
 
 class ApiDeliveryServices:
 
-    @staticmethod
-    def delivery_services_russian_post(connection_type, shop_id, headers,
-                                       token: str =f"{os.getenv('RP_TOKEN')}", secret: str =f"{os.getenv('RP_SECRET')}"):
+    def __init__(self, app):
+        self.app = app
+
+    def delivery_services_russian_post(self, connection_type, shop_id, headers,
+                                       token: str =f"{os.getenv('RP_TOKEN')}",
+                                       secret: str =f"{os.getenv('RP_SECRET')}"):
         """Настройки подключения службы доставки Почты России к магазину"""
         if connection_type == "integration":
             json_russian_post_integration = json.dumps(
@@ -25,7 +28,7 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            integration_russian_post = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            integration_russian_post = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                         data=json_russian_post_integration, headers=headers)
             return integration_russian_post
         elif connection_type == "aggregation":
@@ -38,12 +41,11 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            aggregation_russian_post = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            aggregation_russian_post = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                         data=json_russian_post_aggregation, headers=headers)
             return aggregation_russian_post
 
-    @staticmethod
-    def delivery_services_topdelivery(connection_type: str, shop_id: str, headers: dict):
+    def delivery_services_topdelivery(self, connection_type: str, shop_id: str, headers: dict):
         """Настройки подключения службы доставки TopDelivery к магазину"""
         if connection_type == "integration":
             json_topdelivery_integration = json.dumps(
@@ -58,7 +60,7 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            integration_topdelivery = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            integration_topdelivery = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                        data=json_topdelivery_integration, headers=headers)
             return integration_topdelivery
         elif connection_type == "aggregation":
@@ -70,12 +72,11 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            aggregation_topdelivery = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            aggregation_topdelivery = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                        data=json_topdelivery_aggregation, headers=headers)
             return aggregation_topdelivery
 
-    @staticmethod
-    def delivery_services_boxberry(connection_type: str, shop_id: str, headers: dict):
+    def delivery_services_boxberry(self, connection_type: str, shop_id: str, headers: dict):
         """Настройки подключения службы доставки Boxberry к магазину"""
         if connection_type == "integration":
             json_boxberry_integration = json.dumps(
@@ -87,7 +88,7 @@ class ApiDeliveryServices:
                         "token": "d6f33e419c16131e5325cbd84d5d6000"
                     }
                 })
-            integration_boxberry = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            integration_boxberry = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                     data=json_boxberry_integration, headers=headers)
             return integration_boxberry
         elif connection_type == "aggregation":
@@ -100,12 +101,11 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            aggregation_boxberry = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            aggregation_boxberry = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                     data=json_boxberry_aggregation, headers=headers)
             return aggregation_boxberry
 
-    @staticmethod
-    def delivery_services_cdek(connection_type: str, shop_id: str, headers: dict):
+    def delivery_services_cdek(self, connection_type: str, shop_id: str, headers: dict):
         """Настройки подключения службы доставки Cdek к магазину"""
         if connection_type == "integration":
             json_cdek_integration = json.dumps(
@@ -119,7 +119,7 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            integration_cdek = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            integration_cdek = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                 data=json_cdek_integration, headers=headers)
             return integration_cdek
         elif connection_type == "aggregation":
@@ -132,12 +132,11 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            aggregation_cdek = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            aggregation_cdek = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                 data=json_cdek_aggregation, headers=headers)
             return aggregation_cdek
 
-    @staticmethod
-    def delivery_services_drh_logistic(shop_id: str, headers: dict):
+    def delivery_services_drh_logistic(self, shop_id: str, headers: dict):
         """Настройки подключения службы доставки DRH Logistic к магазину"""
         json_drh_logistic = json.dumps(
             {
@@ -148,12 +147,11 @@ class ApiDeliveryServices:
                 }
             }
         )
-        drh_logistic = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+        drh_logistic = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                         data=json_drh_logistic, headers=headers)
         return drh_logistic
 
-    @staticmethod
-    def delivery_services_dpd(connection_type: str, shop_id: str, headers: dict):
+    def delivery_services_dpd(self, connection_type: str, shop_id: str, headers: dict):
         """Настройки подключения службы доставки Dpd к магазину"""
         if connection_type == "integration":
             json_dpd_integration = json.dumps(
@@ -167,7 +165,7 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            integration_dpd = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            integration_dpd = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                data=json_dpd_integration, headers=headers)
             return integration_dpd
         elif connection_type == "aggregation":
@@ -180,12 +178,11 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            aggregation_dpd = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
+            aggregation_dpd = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
                                                data=json_dpd_aggregation, headers=headers)
             return aggregation_dpd
 
-    @staticmethod
-    def delivery_services_cse(shop_id: str, headers: dict):
+    def delivery_services_cse(self, shop_id: str, headers: dict):
         """Настройки подключения службы доставки Cse к магазину"""
         json_cse = json.dumps(
             {
@@ -197,11 +194,11 @@ class ApiDeliveryServices:
                 }
             }
         )
-        cse = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services", data=json_cse, headers=headers)
+        cse = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services", data=json_cse,
+                                        headers=headers)
         return cse
 
-    @staticmethod
-    def delivery_services_five_post(connection_type: str, shop_id: str, headers: dict):
+    def delivery_services_five_post(self, connection_type: str, shop_id: str, headers: dict):
         """Настройки подключения службы доставки FivePost к магазину"""
         if connection_type == "integration":
             json_five_post_integration = json.dumps(
@@ -215,8 +212,8 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            integration_five_post = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
-                                                     data=json_five_post_integration, headers=headers)
+            integration_five_post = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
+                                                              data=json_five_post_integration, headers=headers)
             return integration_five_post
         elif connection_type == "aggregation":
             json_five_post_aggregation = json.dumps(
@@ -227,12 +224,11 @@ class ApiDeliveryServices:
                     }
                 }
             )
-            aggregation_five_post = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services",
-                                                     data=json_five_post_aggregation, headers=headers)
+            aggregation_five_post = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
+                                                              data=json_five_post_aggregation, headers=headers)
             return aggregation_five_post
 
-    @staticmethod
-    def delivery_services_dostavka_club(shop_id: str, headers: dict):
+    def delivery_services_dostavka_club(self, shop_id: str, headers: dict):
         """Настройки подключения службы доставки DostavkaClub к магазину"""
         json_dostavka_club = json.dumps(
             {
@@ -244,18 +240,17 @@ class ApiDeliveryServices:
                 }
             }
         )
-        dostavka_club = HttpMethods.post(link=f"/customer/shops/{shop_id}/delivery_services", data=json_dostavka_club,
-                                         headers=headers)
+        dostavka_club = self.app.http_method.post(link=f"/customer/shops/{shop_id}/delivery_services",
+                                                  data=json_dostavka_club,
+                                                  headers=headers)
         return dostavka_club
 
-    @staticmethod
-    def get_delivery_services(shop_id: str, headers: dict):
+    def get_delivery_services(self, shop_id: str, headers: dict):
         """Метод получения списка выполненных настроек СД к магазину"""
-        get_delivery_services = HttpMethods.get(link=f"/customer/shops/{shop_id}/delivery_services", headers=headers)
+        get_delivery_services = self.app.http_method.get(link=f"/customer/shops/{shop_id}/delivery_services", headers=headers)
         return get_delivery_services
 
-    @staticmethod
-    def editing_fields_delivery_services(shop_id: str, code: str, headers: dict, value: bool):
+    def editing_fields_delivery_services(self, shop_id: str, code: str, headers: dict, value: bool):
         """Метод изменения полей СД"""
         json_editing_fields_delivery_services = json.dumps(
             [
@@ -266,7 +261,7 @@ class ApiDeliveryServices:
                 }
             ]
         )
-        editing_fields_delivery_services = HttpMethods.patch(
+        editing_fields_delivery_services = self.app.http_method.patch(
             link=f"/v2/customer/shops/{shop_id}/delivery_services/{code}",
             data= json_editing_fields_delivery_services, headers=headers)
         return editing_fields_delivery_services
--- utils/api/orders/orders.py
+++ utils/api/orders/orders.py
@@ -1,12 +1,13 @@
-from utils.http_methods import HttpMethods
 from random import randrange, randint
 import json
 
 
 class ApiOrder:
 
-    @staticmethod
-    def json_order(warehouse_id: str, shop_id: str, payment_type: str, type_ds: str, service: str, tariff: str,
+    def __init__(self, app):
+        self.app = app
+
+    def json_order(self, warehouse_id: str, shop_id: str, payment_type: str, type_ds: str, service: str, tariff: str,
                    price: float, declared_value: float, delivery_point_code=None, weight: float = randint(3, 10),
                    length: float = randint(15, 50), width: float = randint(15, 50), height: float = randint(15, 50)):
         """Json создания заказа"""
@@ -67,72 +68,70 @@ class ApiOrder:
         )
         return json_create_order
 
-    @staticmethod
-    def search_order(params, headers):
+    def search_order(self, params, headers):
         """Метод поиска по заказам"""
-        search_order = HttpMethods.get(link="/orders/search", params=params, headers=headers)
+        search_order = self.app.http_method.get(link="/orders/search", params=params, headers=headers)
         return search_order
 
-    @staticmethod
-    def create_order(warehouse_id, shop_id, payment_type, type_ds, service, tariff, price, declared_value, headers,
+    def create_order(self, warehouse_id, shop_id, payment_type, type_ds, service, tariff, price, declared_value, headers,
                      delivery_point_code=None):
         """Метод создания заказа"""
-        order = ApiOrder.json_order(warehouse_id=warehouse_id, shop_id=shop_id, payment_type=payment_type,
+        order = self.json_order(warehouse_id=warehouse_id, shop_id=shop_id, payment_type=payment_type,
                                     type_ds=type_ds, service=service, tariff=tariff, price=price,
                                     declared_value=declared_value, delivery_point_code=delivery_point_code)
-        result_create_order = HttpMethods.post(link="/orders", data=order, headers=headers)
+        result_create_order = self.app.http_method.post(link="/orders", data=order, headers=headers)
         return result_create_order
 
-    @staticmethod
-    def get_orders(headers: dict):
+    def get_orders(self, headers: dict):
         """Метод возвращает список заказов"""
-        result_get_order_list = HttpMethods.get(link="/orders", headers=headers)
+        result_get_order_list = self.app.http_method.get(link="/orders", headers=headers)
         return result_get_order_list
 
-    @staticmethod
-    def get_order_by_id(headers: dict, order_id: str):
+    def get_order_by_id(self, headers: dict, order_id: str):
         """Метод получения информации о заказе по его id"""
-        result_get_odre_by_id = HttpMethods.get(link=f"/orders/{order_id}", headers=headers)
+        result_get_odre_by_id = self.app.http_method.get(link=f"/orders/{order_id}", headers=headers)
         return result_get_odre_by_id
 
-    @staticmethod
-    def update_order(order_id: str, headers: dict):
+    def update_order(self, order_id: str, headers: dict):
         """Метод обновления заказ"""
         # Todo надо будет дописать метод
         pass
 
-    @staticmethod
-    def update_fields_order(order_id: str, headers: dict):
+    def update_fields_order(self, order_id: str, headers: dict):
         """Метод обновления поля в заказе"""
         # Todo надо будет дописать метод
         pass
 
-    @staticmethod
-    def delete_order(order_id: str, headers: dict):
+    def delete_order(self, order_id: str, headers: dict):
         """Метод удаления заказа"""
-        result_delete_order = HttpMethods.delete(link=f"/orders/{order_id}", headers=headers)
+        result_delete_order = self.app.http_method.delete(link=f"/orders/{order_id}", headers=headers)
         return result_delete_order
 
-    @staticmethod
-    def get_order_patches(order_id: str, headers: dict):
+    def get_order_patches(self, order_id: str, headers: dict):
         """Получение PATCH изменений по заказу"""
-        result_get_order_patches = HttpMethods.get(link=f"/orders/{order_id}/patches", headers=headers)
+        result_get_order_patches = self.app.http_method.get(link=f"/orders/{order_id}/patches", headers=headers)
         return result_get_order_patches
 
-    @staticmethod
-    def get_order_statuses(order_id: str, headers: dict):
+    def get_order_statuses(self, order_id: str, headers: dict):
         """Получение информации об истории изменения статусов заказа"""
-        result_get_order_statuses = HttpMethods.get(link=f"/orders/{order_id}/statuses", headers=headers)
+        result_get_order_statuses = self.app.http_method.get(link=f"/orders/{order_id}/statuses", headers=headers)
         return result_get_order_statuses
 
-    @staticmethod
-    def get_order_details(order_id: str, headers: dict):
+    def get_order_details(self, order_id: str, headers: dict):
         """Получение подробной информации о заказе"""
-        result_get_order_details = HttpMethods.get(link=f"/orders/{order_id}/details", headers=headers)
+        result_get_order_details = self.app.http_method.get(link=f"/orders/{order_id}/details", headers=headers)
         return result_get_order_details
 
-    @staticmethod
-    def get_label(order_id: str, headers: dict):
+    def get_label(self, order_id: str, headers: dict):
         """Метод получения этикетки заказа"""
-        result_get_label = HttpMethods.get(link=f"orders/{order_id}/label", headers=headers)
+        result_get_label = self.app.http_method.get(link=f"/orders/{order_id}/label", headers=headers)
         return result_get_label
+
+    def get_orders_id(self, headers):
+        """Метод получения id заказов не в партии"""
+        orders_id_list = []
+        order_list = self.get_orders(headers=headers)
+        for order in order_list.json():
+            if order["status"] == "created":
+                orders_id_list.append(order["id"])
+        return orders_id_list
