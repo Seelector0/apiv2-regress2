28588a5fb68656a3e74c3d81c90101715e066b26
fox140 <andrey.malcev@pimpay.ru>
2023-02-17 13:17:42 +0300

    Сделал format fidget на будущее

--- utils/api/offers/offers.py
+++ utils/api/offers/offers.py
@@ -6,20 +6,37 @@ class ApiOffers:
     def __init__(self, app):
         self.app = app
 
-    def get_offers(self, warehouse_id, shop_id, payment_type, types, delivery_service_code, headers):
+    def get_offers(self, warehouse_id, shop_id, types, headers, delivery_service_code=None, payment_type=None,
+                   format_=None):
         """Метод для получения списка офферов"""
-        body_offers = {
-            "address":  "г Москва, пр-кт Мира, д 45 стр 2",
-            "declaredValue": randrange(1000, 5000),
-            "height": randrange(10, 45),
-            "length": randrange(10, 45),
-            "width": randrange(10, 45),
-            "weight": randrange(1, 10),
-            "warehouseId": warehouse_id,
-            "shopId": shop_id,
-            "paymentType": payment_type,
-            "types[0]": types,
-            "deliveryServiceCode": delivery_service_code,
-        }
-        result_offers = self.app.http_method.get(link="/offers", params=body_offers, headers=headers)
-        return result_offers
+        if format_ is None:
+            body_offers = {
+                "address":  "г Москва, пр-кт Мира, д 45 стр 2",
+                "declaredValue": randrange(1000, 5000),
+                "height": randrange(10, 45),
+                "length": randrange(10, 45),
+                "width": randrange(10, 45),
+                "weight": randrange(1, 10),
+                "warehouseId": warehouse_id,
+                "shopId": shop_id,
+                "paymentType": payment_type,
+                "types[0]": types,
+                "deliveryServiceCode": delivery_service_code,
+            }
+            result_offers = self.app.http_method.get(link="/offers", params=body_offers, headers=headers)
+            return result_offers
+        elif format_ == "widget":
+            body_offers = {
+                "address": "г Москва, пр-кт Мира, д 45 стр 2",
+                "declaredValue": randrange(1000, 5000),
+                "height": randrange(10, 45),
+                "length": randrange(10, 45),
+                "width": randrange(10, 45),
+                "weight": randrange(1, 10),
+                "warehouseId": warehouse_id,
+                "shopId": shop_id,
+                "types[0]": types,
+                "format": "widget"
+            }
+            result_offers = self.app.http_method.get(link="/offers", params=body_offers, headers=headers)
+            return result_offers
