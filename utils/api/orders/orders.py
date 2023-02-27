ff481ab485377792e55b4ec2ddcdac9f529345d5
fox140 <andrey.malcev@pimpay.ru>
2023-02-27 19:07:49 +0300

    Убрал json создания заказа, написал метод создания многоместного заказа, написал метод создания заказа из файла, переписал метод поиска по заказам, переписал метод update_field_order для добавления места в многоместный заказ.

--- utils/api/orders/orders.py
+++ utils/api/orders/orders.py
@@ -8,12 +8,12 @@ class ApiOrder:
     def __init__(self, app):
         self.app = app
 
-    @staticmethod
-    def json_order(warehouse_id: str, shop_id: str, payment_type: str, type_ds: str, service: str, tariff: str,
-                   price: float, declared_value: float, delivery_point_code=None, weight: float = randint(3, 5),
-                   length: float = randint(10, 30), width: float = randint(10, 50), height: float = randint(10, 50)):
-        """Json создания заказа"""
-        json_create_order = json.dumps(
+    def create_order(self, warehouse_id: str, shop_id: str, payment_type: str, declared_value, type_ds: str,
+                     service: str, tariff: str, price: float, headers: dict, length: float = randint(10, 30),
+                     width: float = randint(10, 50), height: float = randint(10, 50), weight: float = randint(1, 5),
+                     delivery_point_code: str = None, delivery_time: dict = None, sec: float = 4):
+        """Создание заказа"""
+        json_order = json.dumps(
             {
                 "warehouse": {
                     "id": warehouse_id,
@@ -32,16 +32,13 @@ class ApiOrder:
                     "width": width,
                     "height": height
                 },
-                "weight": weight,
+                "weight": 0 + weight,
                 "delivery": {
                     "type": type_ds,
                     "service": service,
                     "tariff": tariff,
-                    "date": "",
-                    "time": {
-                        "from": "",
-                        "to": ""
-                    },
+                    "date": None,
+                    "time": delivery_time,
                     "deliveryPointCode": delivery_point_code
                 },
                 "recipient": {
@@ -59,7 +56,7 @@ class ApiOrder:
                         "items": [
                             {
                                 "article": f"ART{randrange(1000000, 9999999)}",
-                                "name": "111",
+                                "name": "Стол",
                                 "price": price,
                                 "count": 1,
                                 "weight": weight,
@@ -70,24 +67,130 @@ class ApiOrder:
                 ]
             }
         )
-        return json_create_order
-
-    def search_order(self, params, headers):
-        """Метод поиска по заказам"""
-        search_order = self.app.http_method.get(link="/orders/search", params=params, headers=headers)
-        return search_order
-
-    def create_order(self, warehouse_id, shop_id, payment_type, type_ds, service, tariff, price, declared_value, headers,
-                     delivery_point_code=None, length: float = randint(10, 30), width: float = randint(10, 50),
-                     height: float = randint(10, 50), sec: float = 4):
-        """Метод создания заказа"""
-        order = self.json_order(warehouse_id=warehouse_id, shop_id=shop_id, payment_type=payment_type,
-                                type_ds=type_ds, service=service, tariff=tariff, price=price,
-                                declared_value=declared_value, length=length, width=width, height=height,
-                                delivery_point_code=delivery_point_code)
-        result_create_order = self.app.http_method.post(link="/orders", data=order, headers=headers)
+        result_create_order = self.app.http_method.post(link="/orders", data=json_order, headers=headers)
         time.sleep(sec)
         return result_create_order
+
+    def create_multi_order(self, warehouse_id: str, shop_id: str, payment_type: str, declared_value: float,
+                           type_ds: str, service: str, tariff: str, price_1: float,  headers: dict,
+                           delivery_point_code: str = None, delivery_time: dict = None, length: float = randint(10, 30),
+                           width: float = randint(10, 50), height: float = randint(10, 50),
+                           weight_1: float = randint(1, 5), weight_2: float = randint(1, 5), shop_number: str = None,
+                           dimension: dict = None, sec: float = 4):
+        """Создание многоместного заказа"""
+        json_multi_order = json.dumps(
+            {
+                "warehouse": {
+                    "id": warehouse_id
+                },
+                "shop": {
+                    "id": shop_id,
+                    "number": f"{randrange(100000, 999999)}"
+                },
+                "payment": {
+                    "type": payment_type,
+                    "declaredValue": declared_value,
+                    "deliverySum": 200
+                },
+                "dimension": {
+                    "length": length,
+                    "width": width,
+                    "height": height
+                },
+                "weight": weight_1,
+                "delivery": {
+                    "type": type_ds,
+                    "deliveryPointCode": delivery_point_code,
+                    "service": service,
+                    "tariff": tariff,
+                    "date": None,
+                    "time": delivery_time
+                },
+                "recipient": {
+                    "familyName": "Иванов",
+                    "firstName": "Иван",
+                    "secondName": "Иванович",
+                    "phoneNumber": f"+7909{randrange(1000000, 9999999)}",
+                    "address": {
+                        "raw": "603000, Нижегородская обл, г Нижний Новгород, ул Большая Покровская, д 4"
+                    }
+                },
+                "comment": "",
+                "places": [
+                    {
+                        "items": [
+                            {
+                                "article": f"ART1{randrange(1000000, 9999999)}",
+                                "name": "Стол",
+                                "price": price_1,
+                                "count": 1,
+                                "weight": weight_1,
+                                "vat": "NO_VAT"
+                            }
+                        ],
+                        "barcode": f"Box_1{randrange(100000, 999999)}",
+                        "shopNumber": shop_number,
+                        "weight": weight_1,
+                        "dimension": dimension
+                    },
+                    {
+                        "items": [
+                            {
+                                "article": f"ART2{randrange(1000000, 9999999)}",
+                                "name": "Стул",
+                                "price": 1000,
+                                "count": 1,
+                                "weight": weight_2,
+                                "vat": "NO_VAT"
+                            }
+                        ],
+                        "barcode": f"Box_2{randrange(100000, 999999)}",
+                        "shopNumber": shop_number,
+                        "weight": weight_2,
+                        "dimension": dimension
+                    }
+                ]
+            }
+        )
+        result_create_multi_order = self.app.http_method.post(link="/orders", data=json_multi_order, headers=headers)
+        time.sleep(sec)
+        return result_create_multi_order
+
+    def create_order_from_file(self, shop_id: str, warehouse_id: str, headers: dict, format_file: str = None):
+        """Метод создания заказа из файла XLSX формата Почта России или формата MetaShip"""
+        if format_file == "russian_post":
+            body_requests = {
+                "shopId": shop_id,
+                "warehouseId": warehouse_id,
+                "type": "russian_post"
+            }
+            file = [
+                ("file", ("ПочтаПартия_20220614_.xls", open("/home/fox140/тесты/ПочтаПартия_20220614_.xls", "rb"),
+                          "application/vnd.ms-excel"))
+            ]
+            result_order_from_file_russian_post = self.app.http_method.post(link="/import/orders", data=body_requests,
+                                                                            files=file, headers=headers)
+            return result_order_from_file_russian_post
+        else:
+            body_requests = {
+                "shopId": shop_id,
+                "warehouseId": warehouse_id
+            }
+            file = [
+                ("file", ("ПочтаПартия_20220614_.xls", open("/home/fox140/тесты/ПочтаПартия_20220614_.xls", "rb"),
+                          "application/vnd.ms-excel"))
+            ]
+            result_order_from_file_metaship = self.app.http_method.post(link="/import/orders", data=body_requests,
+                                                                        files=file, headers=headers)
+            return result_order_from_file_metaship
+
+    def search_order(self, query: str, headers: dict):
+        """Метод поиска по заказам"""
+        search = {
+            "query": query
+        }
+        search_order = self.app.http_method.get(link="/orders/search", params=search, headers=headers)
+        return search_order
 
     def get_orders(self, headers: dict):
         """Метод возвращает список заказов"""
@@ -114,17 +217,42 @@ class ApiOrder:
         result_put_order = self.app.http_method.put(link=f"/orders/{order_id}", data=payload, headers=headers)
         return result_put_order
 
-    def update_field_order(self, order_id: str, path: str, value, headers: dict):
-        """Метод обновления поля в заказе (PATCH)"""
-        json_path_order = json.dumps(
-            {
-                "op": "replace",
-                "path": path,
-                "value": value
-            }
-        )
-        result_path = self.app.http_method.patch(link=f"/v2/orders/{order_id}", data=json_path_order, headers=headers)
-        return result_path
+    def update_field_order(self, order_id: str, path: str, headers: dict, shop_number: str = None,
+                           weight_3: float = randint(1, 5), dimension: dict = None, sec=1):
+        """Метод обновления поля в заказе и добавление места (PATCH)"""
+        result_get_order_by_id = self.get_order_by_id(order_id=order_id, headers=headers)
+        items_1 = result_get_order_by_id.json()["data"]["request"]["places"]
+        if path == "places":
+            json_path_order = json.dumps(
+                [
+                    {
+                        "op": "replace",
+                        "path": "places",
+                        "value": [
+                            *items_1,
+                            {
+                                "items": [
+                                    {
+                                        "article": f"ART3{randrange(1000000, 9999999)}",
+                                        "name": "Пуфик",
+                                        "price": 1000,
+                                        "count": 1,
+                                        "weight": weight_3,
+                                        "vat": "NO_VAT"
+                                    }
+                                ],
+                                "barcode": f"Box_3{randrange(100000, 999999)}",
+                                "shopNumber": shop_number,
+                                "weight": weight_3,
+                                "dimension": dimension
+                            }
+                        ]
+                    }
+                ]
+            )
+            time.sleep(sec)
+            result_path = self.app.http_method.patch(link=f"/orders/{order_id}", data=json_path_order, headers=headers)
+            return result_path
 
     def delete_order(self, order_id: str, headers: dict):
         """Метод удаления заказа"""
