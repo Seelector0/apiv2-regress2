a9ced6d4828e869bba3dc33c9f3227393c37ec55
fox140 <andrey.malcev@pimpay.ru>
2023-03-02 16:52:07 +0300

    Добавил импорты, сделал метод для чистки директории с логами

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from utils.api.delivery_serviches.delivery_services import ApiDeliveryServices
 from utils.api.documents.documents import ApiDocument
+from utils.logger import Logger
 from utils.api.info.info import ApiInfo
 from utils.api.offers.offers import ApiOffers
 from utils.api.orders.orders import ApiOrder
@@ -9,12 +10,15 @@ from utils.api.webhooks.webhooks import ApiWebhook
 from utils.api.widgets.widgets import ApiWidget
 from utils.http_methods import HttpMethod
 from utils.api.shops.shops import ApiShop
+from pathlib import Path
 import requests
+import os
 
 
 class Application:
 
     def __init__(self, base_url=None, response=None):
+        self.logs_directory = str(Path(Path.home(), "PycharmProjects", "Apiv2-regress-version2", "logs"))
         self.base_url = base_url
         self.session = requests.Session()
         self.response = response
@@ -32,10 +36,15 @@ class Application:
         self.webhook = ApiWebhook(self)
 
     def open_session(self, data, headers: dict):
-        """Функция для открытия сессии"""
+        """Метод для открытия сессии"""
         self.response = self.session.post(url=self.base_url, data=data, headers=headers)
         return self.response
 
+    def clearing_directory(self):
+        """Метод для чистки директории с логами"""
+        for file in os.listdir(self.logs_directory):
+            os.remove(os.path.join(self.logs_directory, file))
+
     def close_session(self):
-        """Функция для закрытия сессии"""
+        """Метод для закрытия сессии"""
         self.session.close()
