48d2559dc1d697db2b5ec4b0f5bfadbdc6d478cb
fox140 <andrey.malcev@pimpay.ru>
2023-03-03 15:50:50 +0300

    Убрал классовый метод также также переписал методы.

--- utils/logger.py
+++ utils/logger.py
@@ -5,15 +5,15 @@ import os
 
 class Logger:
 
-    file_name = f"logs/log_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+    def __init__(self, app):
+        self.app = app
+        self.file_name = f"{self.app.logs_directory}/log_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 
-    @classmethod
-    def _write_log_to_file(cls, data: str):
-        with open(cls.file_name, mode='a', encoding='utf-8') as logger_file:
+    def _write_log_to_file(self, data: str):
+        with open(self.file_name, mode='a', encoding='utf-8') as logger_file:
             logger_file.write(data)
 
-    @classmethod
-    def add_request(cls, url: str, data: dict, headers: dict, method: str):
+    def add_request(self, url: str, data: dict, headers: dict, method: str):
         test_name = os.environ.get('PYTEST_CURRENT_TEST')
         data_to_add = f"\n-----\n"
         data_to_add += f"Test: {test_name}\n"
@@ -23,14 +23,13 @@ class Logger:
         data_to_add += f"Request data: {data}\n"
         data_to_add += f"Request headers: {headers}\n"
         data_to_add += "\n"
-        cls._write_log_to_file(data_to_add)
+        self._write_log_to_file(data_to_add)
 
-    @classmethod
-    def add_response(cls, response: Response):
+    def add_response(self, response: Response):
         headers_as_dict = dict(response.headers)
         data_to_add = f"Response code: {response.status_code}\n"
         data_to_add += f"Response text: {response.text}\n"
         data_to_add += f"Response headers: {headers_as_dict}\n"
         data_to_add += f"X-Trace-Id: {response.request.headers['x-trace-id']}\n"
         data_to_add += "\n-----\n"
-        cls._write_log_to_file(data_to_add)
+        self._write_log_to_file(data_to_add)
