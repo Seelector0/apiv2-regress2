ea7ce36c8f2bfda4525636d684495f9161b049e0
fox140 <andrey.malcev@pimpay.ru>
2023-03-03 18:29:24 +0300

    Добавил описание

--- utils/logger.py
+++ utils/logger.py
@@ -10,10 +10,12 @@ class Logger:
         self.file_name = f"{self.app.logs_directory}/log_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 
     def _write_log_to_file(self, data: str):
+        """Метод создаёт файл и записывает в него информацию"""
         with open(self.file_name, mode='a', encoding='utf-8') as logger_file:
             logger_file.write(data)
 
     def add_request(self, url: str, data: dict, headers: dict, method: str):
+        """Метод возвращает и записывает requests"""
         test_name = os.environ.get('PYTEST_CURRENT_TEST')
         data_to_add = f"\n-----\n"
         data_to_add += f"Test: {test_name}\n"
@@ -26,6 +28,7 @@ class Logger:
         self._write_log_to_file(data_to_add)
 
     def add_response(self, response: Response):
+        """Метод возвращает и записывает response"""
         headers_as_dict = dict(response.headers)
         data_to_add = f"Response code: {response.status_code}\n"
         data_to_add += f"Response text: {response.text}\n"
