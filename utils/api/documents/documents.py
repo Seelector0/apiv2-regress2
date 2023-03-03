72c1dc25b9257e157162f5b6f92fd9a751991ec0
fox140 <andrey.malcev@pimpay.ru>
2023-03-03 15:48:53 +0300

    Расширил метод получения этикеток

--- utils/api/documents/documents.py
+++ utils/api/documents/documents.py
@@ -6,20 +6,28 @@ class ApiDocument:
     def __init__(self, app):
         self.app = app
 
-    def get_label(self, order_id: str, headers: dict):
-        """Метод получения этикетки заказа"""
-        result_get_label = self.app.http_method.get(link=f"/orders/{order_id}/label", headers=headers)
-        return result_get_label
+    def get_label(self, order_id: str, headers: dict, type_: str = None):
+        """Метод получения этикетки"""
+        if type_ == "termo":
+            params = {
+                "type": "termo"
+            }
+            result_get_label = self.app.http_method.get(link=f"/orders/{order_id}/label", params=params,
+                                                        headers=headers)
+            return result_get_label
+        else:
+            result_get_label = self.app.http_method.get(link=f"/orders/{order_id}/label", headers=headers)
+            return result_get_label
 
     def get_labels_from_parcel(self, parcel_id: str, order_ids: list, headers: dict):
         """Метод получения этикеток из партии"""
-        json_get_labels_from_parcel = json.dumps(
+        json_labels_from_parcel = json.dumps(
             {
-                "orderIds": [order_ids]
+                "orderIds": order_ids
             }
         )
         result_get_labels_from_parcel = self.app.http_method.post(link=f"/parcels/{parcel_id}/labels",
-                                                                  data=json_get_labels_from_parcel, headers=headers)
+                                                                  data=json_labels_from_parcel, headers=headers)
         return result_get_labels_from_parcel
 
     def get_app(self, parcel_id, headers):
