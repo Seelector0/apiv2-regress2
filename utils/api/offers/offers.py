2fee788f338a2ff647a35b92110d9b084fa58e39
fox140 <andrey.malcev@pimpay.ru>
2023-03-07 21:02:08 +0300

    Сделал автоматическое подставление shop_id и warehouse_id

--- utils/api/offers/offers.py
+++ utils/api/offers/offers.py
@@ -6,9 +6,10 @@ class ApiOffers:
     def __init__(self, app):
         self.app = app
 
-    def get_offers(self, warehouse_id: str, shop_id: str, types: str, delivery_service_code: str = None,
-                   payment_type: str = None, format_: str = None):
+    def get_offers(self, types: str, delivery_service_code: str = None, payment_type: str = None, format_: str = None):
         """Метод для получения списка офферов"""
+        shop_id = self.app.shop.get_shops_id()
+        warehouse_id = self.app.warehouse.get_warehouses_id()
         if format_ == "widget":
             body_offers = {
                 "address": "г Москва, пр-кт Мира, д 45 стр 2",
@@ -17,8 +18,8 @@ class ApiOffers:
                 "length": randrange(10, 45),
                 "width": randrange(10, 45),
                 "weight": randrange(1, 10),
-                "warehouseId": warehouse_id,
-                "shopId": shop_id,
+                "warehouseId": warehouse_id[0],
+                "shopId": shop_id[0],
                 "types[0]": types,
                 "format": "widget"
             }
@@ -32,8 +33,8 @@ class ApiOffers:
                 "length": randrange(10, 45),
                 "width": randrange(10, 45),
                 "weight": randrange(1, 10),
-                "warehouseId": warehouse_id,
-                "shopId": shop_id,
+                "warehouseId": warehouse_id[0],
+                "shopId": shop_id[0],
                 "paymentType": payment_type,
                 "types[0]": types,
                 "deliveryServiceCode": delivery_service_code,
