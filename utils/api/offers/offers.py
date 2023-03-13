07d179515d4a407a6f46f45fc4d7dc13069a0a2f
fox140 <andrey.malcev@pimpay.ru>
2023-03-13 17:57:24 +0300

    Добавил в запрос delivery_point_number

--- utils/api/offers/offers.py
+++ utils/api/offers/offers.py
@@ -7,37 +7,39 @@ class ApiOffers:
         self.app = app
         self.link = "/offers"
 
-    def get_offers(self, types: str, delivery_service_code: str = None, payment_type: str = None, format_: str = None):
+    def get_offers(self, types: str, delivery_service_code: str = None, payment_type: str = None,
+                   delivery_point_number: str = None, format_: str = None):
         """Метод для получения списка офферов"""
         shop_id = self.app.shop.get_shops_id()
         warehouse_id = self.app.warehouse.get_warehouses_id()
         if format_ == "widget":
             body_offers = {
+                "warehouseId": warehouse_id[0],
+                "shopId": shop_id[0],
                 "address": "г Москва, пр-кт Мира, д 45 стр 2",
                 "declaredValue": randrange(1000, 5000),
                 "height": randrange(10, 45),
                 "length": randrange(10, 45),
                 "width": randrange(10, 45),
                 "weight": randrange(1, 10),
-                "warehouseId": warehouse_id[0],
-                "shopId": shop_id[0],
                 "types[0]": types,
                 "format": "widget"
             }
             result_offers = self.app.http_method.get(link=self.link, params=body_offers)
         else:
             body_offers = {
+                "warehouseId": warehouse_id[0],
+                "shopId": shop_id[0],
                 "address":  "г Москва, пр-кт Мира, д 45 стр 2",
                 "declaredValue": randrange(1000, 5000),
                 "height": randrange(10, 45),
                 "length": randrange(10, 45),
                 "width": randrange(10, 45),
                 "weight": randrange(1, 10),
-                "warehouseId": warehouse_id[0],
-                "shopId": shop_id[0],
                 "paymentType": payment_type,
                 "types[0]": types,
                 "deliveryServiceCode": delivery_service_code,
+                "deliveryPointNumber": delivery_point_number
             }
             result_offers = self.app.http_method.get(link=self.link, params=body_offers)
         return result_offers
