64c69ec09081c3fdb6fedb26cec3bc4689ec21d8
fox140 <andrey.malcev@pimpay.ru>
2023-03-28 19:08:25 +0300

    Добавил аннотации

--- utils/logger.py
+++ utils/logger.py
@@ -10,7 +10,7 @@ class Logger:
     @classmethod
     def _write_log_to_file(cls, data: str):
         """Метод создаёт файл и записывает в него информацию"""
-        with open(cls.file_name, mode="a", encoding="utf-8") as logger_file:
+        with open(file=cls.file_name, mode="a", encoding="utf-8") as logger_file:
             logger_file.write(data)
 
     @classmethod
