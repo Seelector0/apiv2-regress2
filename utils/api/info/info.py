36a9e64a2abed9ccfad0ce973a719464e09881ef
fox140 <andrey.malcev@pimpay.ru>
2023-03-31 18:46:22 +0300

    Cократил код в месте возвращения, теперь они возврвщают метод запроса

--- utils/api/info/info.py
+++ utils/api/info/info.py
@@ -20,8 +20,7 @@ class ApiInfo:
             params = {
                 "deliveryServiceCode": delivery_service_code,
             }
-        result_delivery_time_schedules = self.app.http_method.get(link="/info/delivery_time_schedules", params=params)
-        return result_delivery_time_schedules
+        return self.app.http_method.get(link="/info/delivery_time_schedules", params=params)
 
     def delivery_service_points(self, delivery_service_code: str, city_raw="г. Москва"):
         shop_id = self.app.shop.getting_list_shop_ids()
@@ -31,25 +30,21 @@ class ApiInfo:
             "shopId": shop_id[0],
             "cityRaw": city_raw
         }
-        result_delivery_service_points = self.app.http_method.get(link="/customer/info/delivery_service_points",
-                                                                  params=params)
-        return result_delivery_service_points
+        return self.app.http_method.get(link="/customer/info/delivery_service_points", params=params)
 
     def info_vats(self, delivery_service_code: str):
         """Получение списка ставок НДС, которые умеет принимать и обрабатывать конкретная СД"""
         params = {
             "deliveryServiceCode": delivery_service_code
         }
-        result_info_vats = self.app.http_method.get(link="/info/vats", params=params)
-        return result_info_vats
+        return self.app.http_method.get(link="/info/vats", params=params)
 
     def intake_offices(self, delivery_service_code: str):
         """Получение списка точек сдачи"""
         params = {
             "deliveryServiceCode": delivery_service_code
         }
-        result_intake_offices = self.app.http_method.get(link="/info/intake_offices", params=params)
-        return result_intake_offices
+        return self.app.http_method.get(link="/info/intake_offices", params=params)
 
     def info_statuses(self):
         """Получение полного актуального списка возможных статусов заказа"""
@@ -58,23 +53,19 @@ class ApiInfo:
 
     def info_delivery_service_services(self, code: str):
         """Получение информации о дополнительных услугах поддерживаемых СД"""
-        result_info_delivery_service_services = self.app.http_method.get(link=f"/info/delivery_service/{code}/services")
-        return result_info_delivery_service_services
+        return self.app.http_method.get(link=f"/info/delivery_service/{code}/services")
 
     def user_clients(self):
         """Получение списка ключей"""
-        result_user_clients = self.app.http_method.get(link="/user/clients")
-        return result_user_clients
+        return self.app.http_method.get(link="/user/clients")
 
     def user_clients_id(self, user_id: str):
         """Получение информации о ключе подключения по id"""
-        result_user_clients_id = self.app.http_method.get(link=f"/user/clients/{user_id}")
-        return result_user_clients_id
+        return self.app.http_method.get(link=f"/user/clients/{user_id}")
 
     def info_address(self, raw: str = "101000, г Москва"):
         """Разбор адреса"""
         params = {
             "raw": raw
         }
-        result_info_address = self.app.http_method.get(link="/info/address", parsms=params)
-        return result_info_address
+        return self.app.http_method.get(link="/info/address", parsms=params)
