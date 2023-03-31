ffca6f6a114f290e9b8280b49f0e4bcb804e5bfd
fox140 <andrey.malcev@pimpay.ru>
2023-03-31 18:47:20 +0300

    Cократил код в месте возвращения, теперь они возврвщают метод запроса

--- utils/api/parcels/parcels.py
+++ utils/api/parcels/parcels.py
@@ -24,18 +24,15 @@ class ApiParcel:
                     "shipmentDate": f"{datetime.date.today()}"
                 }
             )
-        result_post_parcel = self.app.http_method.post(link=self.link, data=json_create_parcel)
-        return result_post_parcel
+        return self.app.http_method.post(link=self.link, data=json_create_parcel)
 
     def get_parcels(self):
         """Метод получения списка партий"""
-        result_get_parcels = self.app.http_method.get(link=self.link)
-        return result_get_parcels
+        return self.app.http_method.get(link=self.link)
 
     def get_parcel_id(self, parcel_id: str):
         """Получение партии по её id"""
-        result_get_parcel_by_id = self.app.http_method.get(link=f"{self.link}/{parcel_id}")
-        return result_get_parcel_by_id
+        return self.app.http_method.get(link=f"{self.link}/{parcel_id}")
 
     def patch_parcel(self, op: str, order_id: str, parcel_id: str):
         """Метод редактирования партии - добавление(add) или удаление(remove) заказов"""
@@ -61,8 +58,7 @@ class ApiParcel:
             )
         else:
             raise ValueError(f"Выбран не верная операция {op}, выберите add или remove")
-        result_patch_parcel = self.app.http_method.patch(link=f"{self.link}/{parcel_id}", data=json_patch_parcel)
-        return result_patch_parcel
+        return self.app.http_method.patch(link=f"{self.link}/{parcel_id}", data=json_patch_parcel)
 
     def patch_parcel_shipment_date(self, parcel_id: str, day: int):
         """Метод изменения даты доставки партии"""
@@ -77,8 +73,7 @@ class ApiParcel:
                 }
             ]
         )
-        result_patch_parcel = self.app.http_method.patch(link=f"{self.link}/{parcel_id}", data=json_patch_parcel)
-        return result_patch_parcel
+        return self.app.http_method.patch(link=f"{self.link}/{parcel_id}", data=json_patch_parcel)
 
     def get_orders_in_parcel(self, parcel_id):
         """Получение списка заказов в партии"""
