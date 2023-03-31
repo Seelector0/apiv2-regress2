5589625ad13931fe0b7cf69ec6e61b5211aa62c4
fox140 <andrey.malcev@pimpay.ru>
2023-03-31 18:45:28 +0300

    вывел метод получкуния id партий в преименованный метод link_parcels, также сократил код в месте возвращения, теперь они возврвщают метод

--- utils/api/documents/documents.py
+++ utils/api/documents/documents.py
@@ -6,43 +6,35 @@ class ApiDocument:
     def __init__(self, app):
         self.app = app
 
-    @staticmethod
-    def link(parcel_id):
-        return f"/parcels/{parcel_id}"
+    def link_parcels(self):
+        parcels_ids = self.app.parcel.getting_list_of_parcels_ids()
+        for parcel_id in parcels_ids:
+            return f"/parcels/{parcel_id}"
 
-    def get_label(self, order_id: str, type_: str = None):
+    def get_label(self, order_id: str, type_: str = "original"):
         """Метод получения этикетки"""
         link = f"/orders/{order_id}/label"
         if type_ == "termo":
             params = {
                 "type": "termo"
             }
-            result_get_label = self.app.http_method.get(link=link, params=params)
+            return self.app.http_method.get(link=link, params=params)
         else:
-            result_get_label = self.app.http_method.get(link=link)
-        return result_get_label
+            return self.app.http_method.get(link=link)
 
-    def get_labels_from_parcel(self, parcel_id: str, order_ids: list):
+    def get_labels_from_parcel(self, order_ids: list):
         """Метод получения этикеток из партии"""
         json_labels_from_parcel = json.dumps(
             {
                 "orderIds": order_ids
             }
         )
-        result_get_labels_from_parcel = self.app.http_method.post(link=f"{self.link(parcel_id=parcel_id)}/labels",
-                                                                  data=json_labels_from_parcel)
-        return result_get_labels_from_parcel
+        return self.app.http_method.post(link=f"{self.link_parcels()}/labels", data=json_labels_from_parcel)
 
     def get_app(self):
         """Метод получения АПП"""
-        parcels_ids = self.app.parcel.getting_list_of_parcels_ids()
-        for parcel_id in parcels_ids:
-            result_get_app = self.app.http_method.get(link=f"{self.link(parcel_id=parcel_id)}/acceptance")
-            return result_get_app
+        return self.app.http_method.get(link=f"{self.link_parcels()}/acceptance")
 
     def get_documents(self):
         """Получение документов"""
-        parcels_ids = self.app.parcel.getting_list_of_parcels_ids()
-        for parcel_id in parcels_ids:
-            result_get_files = self.app.http_method.get(link=f"{self.link(parcel_id=parcel_id)}/files")
-            return result_get_files
+        return self.app.http_method.get(link=f"{self.link_parcels()}/files")
