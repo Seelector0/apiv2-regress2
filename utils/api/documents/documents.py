8a3546950478573a2a77271ddd0a3db3613a5195
fox140 <andrey.malcev@pimpay.ru>
2023-04-03 16:59:57 +0300

    Переименовал методы и передела метод получения этикетки

--- utils/api/documents/documents.py
+++ utils/api/documents/documents.py
@@ -11,18 +11,22 @@ class ApiDocument:
         for parcel_id in parcels_ids:
             return f"/parcels/{parcel_id}"
 
-    def get_label(self, order_id: str, type_: str = "original"):
+    def get_label(self, order_id: str, type_: str):
         """Метод получения этикетки"""
         link = f"/orders/{order_id}/label"
         if type_ == "termo":
             params = {
                 "type": "termo"
             }
-            return self.app.http_method.get(link=link, params=params)
+        elif type_ is None or type_ == "original":
+            params = {
+                "type": "original"
+            }
         else:
-            return self.app.http_method.get(link=link)
+            raise ValueError(f"Выбран не верный тип этикетки {type_}, выберите termo или original")
+        return self.app.http_method.get(link=link, params=params)
 
-    def get_labels_from_parcel(self, order_ids: list):
+    def post_labels(self, order_ids: list):
         """Метод получения этикеток из партии"""
         json_labels_from_parcel = json.dumps(
             {
@@ -31,10 +35,10 @@ class ApiDocument:
         )
         return self.app.http_method.post(link=f"{self.link_parcels()}/labels", data=json_labels_from_parcel)
 
-    def get_app(self):
+    def get_acceptance(self):
         """Метод получения АПП"""
         return self.app.http_method.get(link=f"{self.link_parcels()}/acceptance")
 
-    def get_documents(self):
+    def get_files(self):
         """Получение документов"""
         return self.app.http_method.get(link=f"{self.link_parcels()}/files")
