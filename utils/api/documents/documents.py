3d103b2fdb7072c19d340363b41d0d5095ebe36a
fox140 <andrey.malcev@pimpay.ru>
2023-04-04 15:07:45 +0300

    Поменял метод получения ссылки документов и переименовал его

--- utils/api/documents/documents.py
+++ utils/api/documents/documents.py
@@ -6,14 +6,13 @@ class ApiDocument:
     def __init__(self, app):
         self.app = app
 
-    def link_parcels(self):
-        parcels_ids = self.app.parcel.getting_list_of_parcels_ids()
-        for parcel_id in parcels_ids:
-            return f"/parcels/{parcel_id}"
+    def link_documents(self):
+        """Метод получения ссылки для документов"""
+        return f"parcels/{self.app.parcel.getting_list_of_parcels_ids()[0]}"
 
     def get_label(self, order_id: str, type_: str = None):
         """Метод получения этикетки"""
-        link = f"/orders/{order_id}/label"
+        link = f"orders/{order_id}/label"
         if type_ == "termo":
             params = {
                 "type": "termo"
@@ -33,12 +32,12 @@ class ApiDocument:
                 "orderIds": order_ids
             }
         )
-        return self.app.http_method.post(link=f"{self.link_parcels()}/labels", data=json_labels_from_parcel)
+        return self.app.http_method.post(link=f"{self.link_documents()}/labels", data=json_labels_from_parcel)
 
     def get_acceptance(self):
         """Метод получения АПП"""
-        return self.app.http_method.get(link=f"{self.link_parcels()}/acceptance")
+        return self.app.http_method.get(link=f"{self.link_documents()}/acceptance")
 
     def get_files(self):
         """Получение документов"""
-        return self.app.http_method.get(link=f"{self.link_parcels()}/files")
+        return self.app.http_method.get(link=f"{self.link_documents()}/files")
