343513d855a9bd52f46e850b45a798fe00e83914
fox140 <andrey.malcev@pimpay.ru>
2023-04-05 20:41:29 +0300

    Перенёс данные для открытия сессии, переделал метод открытия сессии

--- fixture/application.py
+++ fixture/application.py
@@ -9,6 +9,7 @@ from utils.api.webhooks.webhooks import ApiWebhook
 from utils.api.widgets.widgets import ApiWidget
 from utils.http_methods import HttpMethod
 from utils.api.shops.shops import ApiShop
+from environment import ENV_OBJECT
 from requests import Response
 import requests
 
@@ -18,9 +19,16 @@ class Application:
     def __init__(self, base_url: str = None, response: Response = None, token: dict = None):
         self.base_url = base_url
         self.session = requests.Session()
+        self.data = {
+            "grant_type": "client_credentials",
+            "client_id": f"{ENV_OBJECT.client_id()}",
+            "client_secret": f"{ENV_OBJECT.client_secret()}"
+        }
         self.response = response
         self.token = token
-        self.headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self.headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
         self.http_method = HttpMethod(self)
         self.info = ApiInfo(self)
         self.shop = ApiShop(self)
@@ -33,9 +41,9 @@ class Application:
         self.widget = ApiWidget(self)
         self.webhook = ApiWebhook(self)
 
-    def open_session(self, data: dict, headers: dict):
+    def open_session(self):
         """Метод для открытия сессии"""
-        self.response = self.session.post(url=self.base_url, data=data, headers=headers)
+        self.response = self.session.post(url=self.base_url, data=self.data, headers=self.headers)
         return self.response
 
     def close_session(self):
