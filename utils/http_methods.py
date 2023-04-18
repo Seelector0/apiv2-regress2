5481899d4dac6a3ba624bffd84f21d53a4c695f9
fox140 <andrey.malcev@pimpay.ru>
2023-04-18 14:45:21 +0300

    Добавил в метод POST отправки файла

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -13,9 +13,9 @@ class HttpMethod:
         with allure.step(f"GET requests to URL '{link}'"):
             return self._send(link=link, data=params, headers=self.app.token, method='GET')
 
-    def post(self, link: str, data=None):
+    def post(self, link: str, data=None, files=None):
         with allure.step(f"POST requests to URL '{link}'"):
-            return self._send(link=link, data=data, headers=self.app.token, method='POST')
+            return self._send(link=link, data=data, headers=self.app.token, method='POST', files=files)
 
     def patch(self, link: str, data=None):
         with allure.step(f"POST requests to URL '{link}'"):
@@ -30,13 +30,13 @@ class HttpMethod:
             return self._send(link=link, data=data, headers=self.app.token, method='DELETE')
 
     @staticmethod
-    def _send(link: str, data, headers: dict, method: str):
+    def _send(link: str, data, headers: dict, method: str, files=None):
         link = f"{ENV_OBJECT.get_base_url()}/v2/{link}"
         # Logger.add_request(link, data, headers, method)
         if method == 'GET':
             response = requests.get(url=link, params=data, headers=headers)
         elif method == 'POST':
-            response = requests.post(url=link, data=data, headers=headers)
+            response = requests.post(url=link, data=data, headers=headers, files=files)
         elif method == 'PATCH':
             response = requests.patch(url=link, data=data, headers=headers)
         elif method == 'PUT':
