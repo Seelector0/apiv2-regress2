da47909e8c82bbffa1f6196d3ea876ed56c1dcc5
fox140 <andrey.malcev@pimpay.ru>
2023-04-26 20:19:05 +0300

    Добавил size и format_ для получения original этикеток формата А4, А5 и A6 для СД Сdek и Dpd

--- utils/api/documents/documents.py
+++ utils/api/documents/documents.py
@@ -10,10 +10,12 @@ class ApiDocument:
         """Метод получения ссылки для документов."""
         return f"{self.app.parcel.link}/{self.app.parcel.getting_list_of_parcels_ids()[0]}"
 
-    def get_label(self, order_id: str, type_: str = None):
+    def get_label(self, order_id: str, type_: str = None, size: bool = False, format_: str = None):
         r"""Метод получения этикетки.
         :param order_id: Идентификатор заказа.
         :param type_: Тип этикетки 'original' - этикетка от службы доставки, 'termo' - Этикетка по стандарту MetaShip.
+        :param size: Размер этикетки для этикеток с типом original для СД Cdek и Dpd.
+        :param format_: Формат размера этикеток A4, A5 и A6.
         """
         link = f"{self.app.order.link}/{order_id}/label"
         if type_ == "termo":
@@ -23,6 +25,10 @@ class ApiDocument:
         elif type_ == "original":
             params = {
                 "type": "original"
+            }
+        elif size:
+            params = {
+                "size": f"{format_}"
             }
         else:
             return self.app.http_method.get(link=link)
