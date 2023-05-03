22240051d3081f8ca8d553722bcafad3a9df5e97
fox140 <andrey.malcev@pimpay.ru>
2023-05-03 19:21:14 +0300

    Передел метод получения курсора

--- databases/database_tracking_api.py
+++ databases/database_tracking_api.py
@@ -1,13 +1,15 @@
-from fixture.database import DataBase
 
 
-class DataBaseTrackingApi(DataBase):
+class DataBaseTrackingApi:
+
+    def __init__(self, tracking):
+        self.tracking = tracking
 
     def delete_orders_list_in_tracking(self, order_id):
         r"""Функция чистит таблицу 'public.order'.
         :param order_id: ID заказа в БД.
         """
-        cursor = self.connection_open().cursor()
+        cursor = self.tracking.connection_open().cursor()
         try:
             cursor.execute('DELETE FROM "tracking-api".public."order" '
                            f"""WHERE "order".order_id = '{order_id}'""")
