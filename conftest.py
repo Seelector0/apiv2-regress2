e1c60564b6347ec3c30f58afc6e99d638ecb765f
fox140 <andrey.malcev@pimpay.ru>
2023-05-03 19:17:16 +0300

    Сделал фикстуры БД глобальными, переделал способ подключения к ним через общий метод, поправил метод чистки БД

--- conftest.py
+++ conftest.py
@@ -1,7 +1,5 @@
 from fixture.application import Application
-from databases.database_connections import DataBaseConnections
-from databases.database_customer_api import DataBaseCustomerApi
-from databases.database_tracking_api import DataBaseTrackingApi
+from fixture.database import DataBase
 from utils.checking import Checking
 from environment import ENV_OBJECT
 import pytest
@@ -9,7 +7,9 @@ import uuid
 
 
 fixture_api = None
-fixture_database = None
+fixture_connections = None
+fixture_customer = None
+fixture_tracking = None
 
 
 @pytest.fixture(scope="module")
@@ -34,32 +34,33 @@ def token():
 
 
 @pytest.fixture(scope="module")
-def customer_api():
-    """Фикстура для подключения к базе данных 'customer-api'"""
-    database_customer = DataBaseCustomerApi(host=ENV_OBJECT.host(), database=ENV_OBJECT.db_customer_api(),
-                                            user=ENV_OBJECT.db_connections(), password=ENV_OBJECT.password())
-    database_customer.connection_open()
-    return database_customer
+def connections():
+    """Фикстура для подключения к базе данных 'connections' для dev stage или 'metaship для local stage'"""
+    global fixture_connections
+    if fixture_connections is None:
+        fixture_connections = DataBase(database=ENV_OBJECT.db_connections())
+    fixture_connections.connection_open()
+    return fixture_connections
 
 
 @pytest.fixture(scope="module")
-def connections():
-    """Фикстура для подключения к базе данных 'connections' для dev stage или 'metaship для local stage'"""
-    global fixture_database
-    if fixture_database is None:
-        fixture_database = DataBaseConnections(host=ENV_OBJECT.host(), database=ENV_OBJECT.db_connections(),
-                                               user=ENV_OBJECT.db_connections(), password=ENV_OBJECT.password())
-    fixture_database.connection_open()
-    return fixture_database
+def customer_api():
+    """Фикстура для подключения к базе данных 'customer-api'"""
+    global fixture_customer
+    if fixture_customer is None:
+        fixture_customer = DataBase(database=ENV_OBJECT.db_customer_api())
+    fixture_customer.connection_open()
+    return fixture_customer
 
 
 @pytest.fixture(scope="module")
 def tracking_api():
     """Фикстура для подключения к базе данных 'tracking-api'"""
-    database_tracking = DataBaseTrackingApi(host=ENV_OBJECT.host(), database=ENV_OBJECT.db_tracking_api(),
-                                            user=ENV_OBJECT.db_connections(), password=ENV_OBJECT.password())
-    database_tracking.connection_open()
-    return database_tracking
+    global fixture_tracking
+    if fixture_tracking is None:
+        fixture_tracking = DataBase(database=ENV_OBJECT.db_tracking_api())
+    fixture_tracking.connection_open()
+    return fixture_tracking
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -67,11 +68,11 @@ def stop(app, request, connections, customer_api, tracking_api):
     """Фикстура для завершения сессии"""
     def fin():
         app.close_session()
-        for i in connections.get_shops_list():
-            customer_api.delete_connection(shop_id=i.shop_id)
-        for i in connections.get_orders_list():
-            tracking_api.delete_orders_list_in_tracking(order_id=i.order_id)
-        connections.delete_all_setting()
+        for i in connections.metaship.get_shops_list():
+            customer_api.customer.delete_connection(shop_id=i.shop_id)
+        for i in connections.metaship.get_orders_list():
+            tracking_api.tracking.delete_orders_list_in_tracking(order_id=i.order_id)
+        connections.metaship.delete_all_setting()
         connections.connection_close()
         customer_api.connection_close()
         tracking_api.connection_close()
