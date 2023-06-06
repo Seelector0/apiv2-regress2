e45326816e2192e46c71f609a872e03f2c93c040
fox140 <andrey.malcev@pimpay.ru>
2023-06-06 11:00:56 +0300

    fix

--- utils/api/delivery_serviches/delivery_services.py
+++ utils/api/delivery_serviches/delivery_services.py
@@ -334,6 +334,7 @@ class ApiDeliveryServices:
                     "deliveryServiceCode": "Dalli",
                     "data": {
                         "type": "integration",
+                        "token": f""
                     }
                 }
             )
