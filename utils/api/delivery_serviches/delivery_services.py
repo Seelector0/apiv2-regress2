015ac7eb2e1a0306a922d1e735d9c5880fa5f21f
fox140 <andrey.malcev@pimpay.ru>
2023-07-06 11:30:42 +0300

    Объединил 2 функции в 1, заменил её integration и aggregation

--- utils/api/delivery_serviches/delivery_services.py
+++ utils/api/delivery_serviches/delivery_services.py
@@ -15,30 +15,21 @@ class ApiDeliveryServices:
         self.database = DataBase(database=ENV_OBJECT.db_connections())
 
     @staticmethod
-    def integration(delivery_service_code):
-        r"""Настройки по интеграции.
-        :param delivery_service_code: Код службы доставки.
+    def connection_type(delivery_service_code: str, aggregation: bool = None):
+        r"""Тип подключения СД.
+        :param delivery_service_code: Название СД.
+        :param aggregation: Признак того, что настройка выполнена или выполняется на агрегацию.
         """
-        integration = {
+        connection_type = {
             "deliveryServiceCode": delivery_service_code,
             "data": {
-                "type": "integration"
             }
         }
-        return integration
-
-    @staticmethod
-    def aggregation(delivery_service_code):
-        r"""Настройки по агрегации.
-        :param delivery_service_code: Код службы доставки.
-        """
-        aggregation = {
-            "deliveryServiceCode": delivery_service_code,
-            "data": {
-                "type": "aggregation"
-            }
-        }
-        return aggregation
+        if aggregation is True:
+            connection_type["data"]["type"] = "aggregation"
+        else:
+            connection_type["data"]["type"] = "integration"
+        return connection_type
 
     def link_delivery_services(self):
         """Метод получения ссылки для подключения СД."""
@@ -49,10 +40,10 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            russian_post = self.aggregation(delivery_service_code="RussianPost")
+            russian_post = self.connection_type(delivery_service_code="RussianPost", aggregation=True)
             russian_post["data"]["intakePostOfficeCode"] = "101000"
         else:
-            russian_post = self.integration(delivery_service_code="RussianPost")
+            russian_post = self.connection_type(delivery_service_code="RussianPost")
             russian_post["data"]["intakePostOfficeCode"] = "101000"
             russian_post["data"]["token"] = f"{os.getenv('RP_TOKEN')}"
             russian_post["data"]["secret"] = f"{os.getenv('RP_SECRET')}"
@@ -66,9 +57,9 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            topdelivery = self.aggregation(delivery_service_code="TopDelivery")
+            topdelivery = self.connection_type(delivery_service_code="TopDelivery", aggregation=True)
         else:
-            topdelivery = self.integration(delivery_service_code="TopDelivery")
+            topdelivery = self.connection_type(delivery_service_code="TopDelivery")
             topdelivery["data"]["username"] = f"{os.getenv('TD_USER_NAME')}"
             topdelivery["data"]["password"] = f"{os.getenv('TD_PASSWORD')}"
             topdelivery["data"]["basicLogin"] = f"{os.getenv('TD_BASIC_LOGIN')}"
@@ -83,10 +74,10 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            boxberry = self.aggregation(delivery_service_code="Boxberry")
+            boxberry = self.connection_type(delivery_service_code="Boxberry", aggregation=True)
             boxberry["data"]["intakeDeliveryPointCode"] = "00127"
         else:
-            boxberry = self.integration(delivery_service_code="Boxberry")
+            boxberry = self.connection_type(delivery_service_code="Boxberry")
             boxberry["data"]["intakeDeliveryPointCode"] = "00127"
             boxberry["data"]["token"] = f"{os.getenv('BB_API_TOKEN')}"
         with allure.step(title=f"Requests: {boxberry}"):
@@ -99,10 +90,10 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            cdek = self.aggregation(delivery_service_code="Cdek")
+            cdek = self.connection_type(delivery_service_code="Cdek", aggregation=True)
             cdek["data"]["shipmentPointCode"] = "AKHT1"
         else:
-            cdek = self.integration(delivery_service_code="Cdek")
+            cdek = self.connection_type(delivery_service_code="Cdek")
             cdek["data"]["shipmentPointCode"] = "AKHT1"
             cdek["data"]["account"] = f"{os.getenv('CDEK_ACCOUNT')}"
             cdek["data"]["password"] = f"{os.getenv('CDEK_PASSWORD')}"
@@ -113,7 +104,7 @@ class ApiDeliveryServices:
 
     def delivery_services_drh_logistic(self):
         """Настройки подключения службы доставки DRH Logistic к магазину."""
-        drh_logistic = self.integration(delivery_service_code="Drhl")
+        drh_logistic = self.connection_type(delivery_service_code="Drhl")
         drh_logistic["data"]["apiKey"] = f"{os.getenv('DRHL_API_TOKEN')}"
         with allure.step(title=f"Requests: {drh_logistic}"):
             result = self.app.http_method.post(link=self.link_delivery_services(), data=json.dumps(drh_logistic))
@@ -125,10 +116,10 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            dpd = self.aggregation(delivery_service_code="Dpd")
+            dpd = self.connection_type(delivery_service_code="Dpd", aggregation=True)
             dpd["data"]["intakePointCode"] = "M16"
         else:
-            dpd = self.integration(delivery_service_code="Dpd")
+            dpd = self.connection_type(delivery_service_code="Dpd")
             dpd["data"]["clientNumber"] = f"{os.getenv('DPD_CLIENT_NUMBER')}"
             dpd["data"]["clientKey"] = f"{os.getenv('DPD_CLIENT_KEY')}"
             dpd["data"]["intakePointCode"] = "M16"
@@ -139,7 +130,7 @@ class ApiDeliveryServices:
 
     def delivery_services_cse(self):
         """Настройки подключения службы доставки Cse к магазину."""
-        cse = self.integration(delivery_service_code="Cse")
+        cse = self.connection_type(delivery_service_code="Cse")
         cse["data"]["login"] = f"{os.getenv('CSE_LOGIN')}"
         cse["data"]["password"] = f"{os.getenv('CSE_PASSWORD')}"
         cse["data"]["token"] = f"{os.getenv('CSE_TOKEN')}"
@@ -153,9 +144,9 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            five_post = self.aggregation(delivery_service_code="FivePost")
+            five_post = self.connection_type(delivery_service_code="FivePost", aggregation=True)
         else:
-            five_post = self.integration(delivery_service_code="FivePost")
+            five_post = self.connection_type(delivery_service_code="FivePost")
             five_post["data"]["apiKey"] = f"{os.getenv('FIVE_POST_API_KEY')}"
             five_post["data"]["partnerNumber"] = f"{os.getenv('FIVE_POST_PARTNER_NUMBER')}"
             five_post["data"]["baseWeight"] = 1000
@@ -166,7 +157,7 @@ class ApiDeliveryServices:
 
     def delivery_services_svyaznoy(self):
         """Настройки подключения службы доставки Svyaznoy к магазину."""
-        svyaznoy = self.integration(delivery_service_code="Svyaznoy")
+        svyaznoy = self.connection_type(delivery_service_code="Svyaznoy")
         svyaznoy["data"]["login"] = f"{os.getenv('SL_LOGIN')}"
         svyaznoy["data"]["password"] = f"{os.getenv('SL_PASSWORD')}"
         with allure.step(title=f"Requests: {svyaznoy}"):
@@ -179,9 +170,9 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            yandex_go = self.aggregation(delivery_service_code="YandexGo")
+            yandex_go = self.connection_type(delivery_service_code="YandexGo", aggregation=True)
         else:
-            yandex_go = self.integration(delivery_service_code="YandexGo")
+            yandex_go = self.connection_type(delivery_service_code="YandexGo")
             yandex_go["data"]["token"] = f"{os.getenv('YANDEX_TOKEN')}"
         with allure.step(title=f"Requests: {yandex_go}"):
             result = self.app.http_method.post(link=self.link_delivery_services(), data=json.dumps(yandex_go))
@@ -193,10 +184,10 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            yandex_delivery = self.aggregation(delivery_service_code="YandexDelivery")
+            yandex_delivery = self.connection_type(delivery_service_code="YandexDelivery", aggregation=True)
             yandex_delivery["data"]["intakePointCode"] = "807655"
         else:
-            yandex_delivery = self.integration(delivery_service_code="YandexDelivery")
+            yandex_delivery = self.connection_type(delivery_service_code="YandexDelivery")
             yandex_delivery["data"]["token"] = f"{os.getenv('YANDEX_TOKEN')}"
             yandex_delivery["data"]["intakePointCode"] = "807655"
         with allure.step(title=f"Requests: {yandex_delivery}"):
@@ -206,7 +197,7 @@ class ApiDeliveryServices:
 
     def delivery_services_dostavka_club(self):
         """Настройки подключения службы доставки DostavkaClub к магазину."""
-        dostavka_club = self.integration(delivery_service_code="DostavkaClub")
+        dostavka_club = self.connection_type(delivery_service_code="DostavkaClub")
         dostavka_club["data"]["login"] = f"{os.getenv('CLUB_LOGIN')}"
         dostavka_club["data"]["pass"] = f"{os.getenv('CLUB_PASSWORD')}"
         with allure.step(title=f"Requests: {dostavka_club}"):
@@ -216,7 +207,7 @@ class ApiDeliveryServices:
 
     def delivery_services_dostavka_guru(self):
         """Настройки подключения службы доставки DostavkaGuru к магазину."""
-        dostavka_guru = self.integration(delivery_service_code="DostavkaGuru")
+        dostavka_guru = self.connection_type(delivery_service_code="DostavkaGuru")
         dostavka_guru["data"]["partnerId"] = int(f"{os.getenv('GURU_PARTNER_ID')}")
         dostavka_guru["data"]["key"] = f"{os.getenv('GURU_KEY')}"
         with allure.step(title=f"Requests: {dostavka_guru}"):
@@ -226,7 +217,7 @@ class ApiDeliveryServices:
 
     def delivery_services_l_post(self):
         """Настройки подключения службы доставки LPost к магазину."""
-        l_post = self.integration(delivery_service_code="LPost")
+        l_post = self.connection_type(delivery_service_code="LPost")
         l_post["data"]["secret"] = f"{os.getenv('L_POST_SECRET')}"
         with allure.step(title=f"Requests: {l_post}"):
             result = self.app.http_method.post(link=self.link_delivery_services(), data=json.dumps(l_post))
@@ -238,9 +229,9 @@ class ApiDeliveryServices:
         :param aggregation: Тип подключения СД по агрегации.
         """
         if aggregation is True:
-            dalli = self.aggregation(delivery_service_code="Dalli")
+            dalli = self.connection_type(delivery_service_code="Dalli", aggregation=True)
         else:
-            dalli = self.integration(delivery_service_code="Dalli")
+            dalli = self.connection_type(delivery_service_code="Dalli")
             dalli["data"]["token"] = f"{os.getenv('DALLI_TOKEN')}"
         with allure.step(title=f"Requests: {dalli}"):
             result = self.app.http_method.post(link=self.link_delivery_services(), data=json.dumps(dalli))
