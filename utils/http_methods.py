5a8c662522e5fd5c710d9af4a320e78e02c283eb
fox140 <andrey.malcev@pimpay.ru>
2023-07-25 12:45:55 +0300

    Вывел {ENV_OBJECT.get_base_url()}/v2/ в переменную

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -8,11 +8,12 @@ class HttpMethod:
 
     def __init__(self, app):
         self.app = app
+        self.url = f"{ENV_OBJECT.get_base_url()}/v2/"
 
     def token(self):
         """Метод получения токена для авторизации."""
         x_trace_id = str(uuid.uuid4())
-        with allure.step(title=f"x-trace-id: {x_trace_id}"):
+        with allure.step(title=f"x-trace-id: '{x_trace_id}'"):
             token = {
                 "x-trace-id": x_trace_id,
                 "Authorization": f"Bearer {self.app.response.json()['access_token']}"
@@ -24,8 +25,8 @@ class HttpMethod:
         :param link: Ссылка на запрос.
         :param params: Тело запроса если нужно.
         """
-        with allure.step(title=f"GET requests to URL '{ENV_OBJECT.get_base_url()}/v2/{link}'"):
-            link = f"{ENV_OBJECT.get_base_url()}/v2/{link}"
+        with allure.step(title=f"GET requests to URL '{self.url}{link}'"):
+            link = f"{self.url}{link}"
             return requests.get(url=link, params=params, headers=self.token())
 
     def post(self, link: str, data=None, files=None):
@@ -35,8 +36,8 @@ class HttpMethod:
         :param files: Передаваемый файл.
         :return:
         """
-        with allure.step(title=f"POST requests to URL '{ENV_OBJECT.get_base_url()}/v2/{link}'"):
-            link = f"{ENV_OBJECT.get_base_url()}/v2/{link}"
+        with allure.step(title=f"POST requests to URL '{self.url}{link}'"):
+            link = f"{self.url}{link}"
             return requests.post(url=link, data=data, headers=self.token(), files=files)
 
     def patch(self, link: str, data=None):
@@ -44,8 +45,8 @@ class HttpMethod:
         :param link: Ссылка на запрос.
         :param data: Тело запроса в формате JSON.
         """
-        with allure.step(title=f"PATCH requests to URL '{ENV_OBJECT.get_base_url()}/v2/{link}'"):
-            link = f"{ENV_OBJECT.get_base_url()}/v2/{link}"
+        with allure.step(title=f"PATCH requests to URL '{self.url}{link}'"):
+            link = f"{self.url}{link}"
             return requests.patch(url=link, data=data, headers=self.token())
 
     def put(self, link: str, data=None):
@@ -53,14 +54,14 @@ class HttpMethod:
         :param link: Ссылка на запрос.
         :param data: Тело запроса в формате JSON.
         """
-        with allure.step(title=f"PUT requests to URL '{ENV_OBJECT.get_base_url()}/v2/{link}'"):
-            link = f"{ENV_OBJECT.get_base_url()}/v2/{link}"
+        with allure.step(title=f"PUT requests to URL '{self.url}{link}'"):
+            link = f"{self.url}{link}"
             return requests.put(url=link, data=data, headers=self.token())
 
     def delete(self, link: str):
         r"""DELETE запрос.
         :param link: Ссылка на запрос.
         """
-        with allure.step(title=f"DELETE requests to URL '{ENV_OBJECT.get_base_url()}/v2/{link}'"):
-            link = f"{ENV_OBJECT.get_base_url()}/v2/{link}"
+        with allure.step(title=f"DELETE requests to URL '{self.url}{link}'"):
+            link = f"{self.url}{link}"
             return requests.delete(url=link, headers=self.token())
