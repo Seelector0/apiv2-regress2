5b479f7673e4e2310fafd6fd103227bdec293e86
fox140 <andrey.malcev@pimpay.ru>
2023-07-28 22:50:55 +0300

    Добавил метод получения connection_id из БД

--- databases/customer_api.py
+++ databases/customer_api.py
@@ -1,17 +1,31 @@
+from psycopg2.extras import DictCursor
 
 
 class DataBaseCustomerApi:
 
-    def __init__(self, customer):
-        self.customer = customer
+    def __init__(self, customer_api):
+        self.customer_api = customer_api
+
+    def get_connections_id(self, shop_id):
+        """Метод получения connection_id из БД."""
+        list_connection_id = []
+        cursor = self.customer_api.connection_open().cursor(cursor_factory=DictCursor)
+        try:
+            cursor.execute(query="""select id from "customer-api".public.connection """
+                                 f"""where connection.shop_id='{shop_id}'""")
+            for row in cursor:
+                list_connection_id.append(*row)
+        finally:
+            cursor.close()
+        return list_connection_id
 
     def delete_connection(self, shop_id):
-        r"""Функция чистит таблицу 'public.connection'.
+        r"""Метод чистит таблицу 'public.connection'.
         :param shop_id: ID магазина в БД.
         """
-        cursor = self.customer.connection_open().cursor()
+        cursor = self.customer_api.connection_open().cursor()
         try:
-            cursor.execute('delete from public.connection '
+            cursor.execute("""delete from "customer-api".public.connection """
                            f"""where connection.shop_id = '{shop_id}'""")
             cursor.connection.commit()
         finally:
