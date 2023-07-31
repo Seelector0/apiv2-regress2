18aa9e4d5223af8b3f18585c8768776b93bf7106
fox140 <andrey.malcev@pimpay.ru>
2023-07-31 12:05:46 +0300

    подкоректировал чутка

--- utils/admin_api/connections_delivery_services/moderation_delivery_services.py
+++ utils/admin_api/connections_delivery_services/moderation_delivery_services.py
@@ -13,7 +13,7 @@ class ApiModerationDeliveryServices:
         self.admin = admin
         self.database_connections = DataBase(database=ENV_OBJECT.db_connections())
         self.database_customer = DataBase(database=ENV_OBJECT.db_customer_api())
-        self.link = "/configurations"
+        self.link = "configurations"
 
     def moderation(self, delivery_service_code):
         r"""Метод снятия СД с модерации.
@@ -37,8 +37,8 @@ class ApiModerationDeliveryServices:
         russian_post = self.moderation(delivery_service_code="RussianPost")
         russian_post["credential"]["token"] = f"{os.getenv('RP_TOKEN')}"
         russian_post["credential"]["secret"] = f"{os.getenv('RP_SECRET')}"
-        result = self.admin.http_method.post(link=self.link, data=russian_post, token=self.admin.admin_token(),
-                                             admin=True)
+        result = self.admin.http_method.post(link=self.link, data=russian_post, admin=True,
+                                             token=self.admin.admin_token())
         with allure.step(title=f"Response: {result.json()}"):
             return result
 
@@ -49,8 +49,8 @@ class ApiModerationDeliveryServices:
         topdelivery["credential"]["password"] = f"{os.getenv('TD_PASSWORD')}"
         topdelivery["credential"]["basicLogin"] = f"{os.getenv('TD_BASIC_LOGIN')}"
         topdelivery["credential"]["basicPassword"] = f"{os.getenv('TD_BASIC_PASSWORD')}"
-        result = self.admin.http_method.post(link=self.link, data=topdelivery, token=self.admin.admin_token(),
-                                             admin=True)
+        result = self.admin.http_method.post(link=self.link, data=topdelivery, admin=True,
+                                             token=self.admin.admin_token())
         with allure.step(title=f"Response: {result.json()}"):
             return result
 
@@ -58,8 +58,8 @@ class ApiModerationDeliveryServices:
         """Снятие с модерации СД Boxberry."""
         boxberry = self.moderation(delivery_service_code="Boxberry")
         boxberry["credential"]["token"] = f"{os.getenv('BB_API_TOKEN')}"
-        result = self.admin.http_method.post(link=self.link, data=boxberry, token=self.admin.admin_token(),
-                                             admin=True)
+        result = self.admin.http_method.post(link=self.link, data=boxberry, admin=True,
+                                             token=self.admin.admin_token())
         with allure.step(title=f"Response: {result.json()}"):
             return result
 
@@ -68,8 +68,7 @@ class ApiModerationDeliveryServices:
         cdek = self.moderation(delivery_service_code="Cdek")
         cdek["credential"]["account"] = f"{os.getenv('CDEK_ACCOUNT')}"
         cdek["credential"]["password"] = f"{os.getenv('CDEK_PASSWORD')}"
-        result = self.admin.http_method.post(link=self.link, data=cdek, token=self.admin.admin_token(),
-                                             admin=True)
+        result = self.admin.http_method.post(link=self.link, data=cdek, admin=True, token=self.admin.admin_token())
         with allure.step(title=f"Response: {result.json()}"):
             return result
 
@@ -78,7 +77,7 @@ class ApiModerationDeliveryServices:
         dpd = self.moderation(delivery_service_code="Dpd")
         dpd["credential"]["clientNumber"] = f"{os.getenv('DPD_CLIENT_NUMBER')}"
         dpd["credential"]["clientKey"] = f"{os.getenv('DPD_CLIENT_KEY')}"
-        result = self.admin.http_method.post(link=self.link, data=dpd, token=self.admin.admin_token(), admin=True)
+        result = self.admin.http_method.post(link=self.link, data=dpd, admin=True, token=self.admin.admin_token())
         with allure.step(title=f"Response: {result.json()}"):
             return result
 
@@ -88,8 +87,8 @@ class ApiModerationDeliveryServices:
         five_post["credential"]["apiKey"] = f"{os.getenv('FIVE_POST_API_KEY')}"
         five_post["credential"]["partnerNumber"] = f"{os.getenv('FIVE_POST_PARTNER_NUMBER')}"
         five_post["credential"]["baseWeight"] = 1000
-        result = self.admin.http_method.post(link=self.link, data=five_post, token=self.admin.admin_token(),
-                                             admin=True)
+        result = self.admin.http_method.post(link=self.link, data=five_post, admin=True,
+                                             token=self.admin.admin_token())
         with allure.step(title=f"Response: {result.json()}"):
             return result
 
@@ -98,8 +97,8 @@ class ApiModerationDeliveryServices:
         yandex_go = self.moderation(delivery_service_code="YandexGo")
         yandex_go["credential"]["yandexGoToken"] = f"{os.getenv('YANDEX_TOKEN')}"
         yandex_go["credential"]["inn"] = "7734381257"
-        result = self.admin.http_method.post(link=self.link, data=yandex_go, token=self.admin.admin_token(),
-                                             admin=True)
+        result = self.admin.http_method.post(link=self.link, data=yandex_go, admin=True,
+                                             token=self.admin.admin_token())
         with allure.step(title=f"Response: {result.json()}"):
             return result
 
@@ -107,6 +106,6 @@ class ApiModerationDeliveryServices:
         """Снятие с модерации СД Dalli"""
         dalli = self.moderation(delivery_service_code="Dalli")
         dalli["data"]["token"] = f"{os.getenv('DALLI_TOKEN')}"
-        result = self.admin.http_method.post(link=self.link, data=dalli, token=self.admin.admin_token(), admin=True)
+        result = self.admin.http_method.post(link=self.link, data=dalli, admin=True, token=self.admin.admin_token())
         with allure.step(title=f"Response: {result.json()}"):
             return result
