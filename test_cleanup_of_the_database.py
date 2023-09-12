f109c233749b54fdef98af3d6d30131943edb8fe
fox140 <andrey.malcev@pimpay.ru>
2023-09-12 18:07:22 +0300

    Переписал метод чистки БД

--- test_cleanup_of_the_database.py
+++ test_cleanup_of_the_database.py
@@ -2,9 +2,9 @@
 
 
 def test_clear_all_database(customer_api, connections, tracking_api, widget_api):
-    for id_ in connections.metaship.get_list_shops():
-        customer_api.customer.delete_connection(shop_id=id_)
-        widget_api.widget.delete_widgets_id(shop_id=id_)
-    for id_ in connections.metaship.get_list_orders():
-        tracking_api.tracking.delete_list_orders_in_tracking(order_id=id_)
-    connections.metaship.delete_all_setting()
+    for id_ in connections.get_list_shops():
+        customer_api.delete_connection(shop_id=id_)
+        widget_api.delete_widgets_id(shop_id=id_)
+    for id_ in connections.get_list_orders():
+        tracking_api.delete_list_orders_in_tracking(order_id=id_)
+    connections.delete_all_setting()
