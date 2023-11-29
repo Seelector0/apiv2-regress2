4608e4482b3eab22e7291098eb450f0405c1fe6e
fox140 <andrey.malcev@pimpay.ru>
2023-11-29 18:14:26 +0300

    Исправил метод создания этикеток в партии

--- utils/apiv2_metaship/forms/forms.py
+++ utils/apiv2_metaship/forms/forms.py
@@ -11,8 +11,7 @@ class Forms:
     def post_forms(self):
         """Создание формы с этикетками партии."""
         forms_url = f"{self.link}/{self.app.parcel.link}/{self.db_connections.get_list_parcels()[0]}/labels"
-        forms = self.app.dict.form_forms_labels()
-        result = self.app.http_method.post(link=forms_url, json=forms)
+        result = self.app.http_method.post(link=forms_url)
         return self.app.http_method.return_result(response=result)
 
     def get_forms(self):
