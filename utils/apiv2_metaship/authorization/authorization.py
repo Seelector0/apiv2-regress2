52969c2efbb16a87d0d0442346253ff581be7713
fox140 <andrey.malcev@pimpay.ru>
2023-11-29 14:32:41 +0300

    Добавил описание.

--- utils/apiv2_metaship/authorization/authorization.py
+++ utils/apiv2_metaship/authorization/authorization.py
@@ -11,6 +11,9 @@ class ApiAuthorization:
         self.session = requests.Session()
 
     def post_access_token(self, admin: bool = None):
+        r"""Метод получение bearer-токена.
+        :param admin: Получение bearer-токена для AdminApi.   
+        """
         data = Dicts.form_authorization(admin=admin)
-        return self.session.post(url=f"{ENV_OBJECT.get_base_url()}/auth/access_token", data=data,
-                                 headers=Dicts.form_headers())
+        headers = Dicts.form_headers()
+        return self.session.post(url=f"{ENV_OBJECT.get_base_url()}/auth/access_token", data=data, headers=headers)
