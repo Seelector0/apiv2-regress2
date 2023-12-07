07718c5e9235bb2c25a1482c22c764c2cbde1916
fox140 <andrey.malcev@pimpay.ru>
2023-12-07 20:04:43 +0300

    Словари для админских методов

---
+++ utils/admin_api/dicts.py
@@ -0,0 +1,38 @@
+from databases.connections import DataBaseConnections
+from databases.customer_api import DataBaseCustomerApi
+from environment import ENV_OBJECT
+
+
+class AdminDicts:
+
+    def __init__(self, admin):
+        self.admin = admin
+        self.db_connections = DataBaseConnections()
+        self.db_customer_api = DataBaseCustomerApi()
+
+    def form_connections_delivery_services(self, delivery_service_code: str):
+        r"""Форма для снятия с модерации СД.
+        :param delivery_service_code: Название СД.
+        """
+        shop_id = self.db_connections.get_list_shops()[0]
+        return {
+            "shopId": shop_id,
+            "customerId": ENV_OBJECT.customer_id(),
+            "connectionId": self.db_customer_api.get_connections_id(shop_id=shop_id)[0],
+            "agreementId": "19852a56-8e10-4516-8218-8acefc2c2bd2",
+            "customerAgreementId": ENV_OBJECT.customer_agreements_id(),
+            "credential": dict(),
+            "deliveryService": delivery_service_code
+        }
+
+    @staticmethod
+    def form_update_connection():
+        """Форма для обновления connection."""
+        return {
+            "data": {
+                "intakePostOfficeCode": "7777",
+                "bin": "123456789012",
+                "counterparty": "888.664",
+                "acceptanceEmail": "test@mail.ru"
+            }
+        }
