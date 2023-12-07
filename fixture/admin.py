4be308bcbe403f935293d3525fc1a153b6b101b9
fox140 <andrey.malcev@pimpay.ru>
2023-12-07 20:05:07 +0300

    Новые импорты и классы

--- fixture/admin.py
+++ fixture/admin.py
@@ -1,13 +1,13 @@
-from utils.admin_api.connections_delivery_services.connections_delivery_services import ApiModerationDeliveryServices
+from utils.admin_api.connections_delivery_services.connections_delivery_services import ApiConnectionDeliveryServices
 from utils.apiv2_metaship.authorization.authorization import ApiAuthorization
+from utils.admin_api.dicts import AdminDicts
 from utils.http_methods import HttpMethod
-from utils.dicts import Dicts
 
 
 class Admin:
 
     def __init__(self):
         self.http_method = HttpMethod(self, self)
+        self.dicts = AdminDicts(self)
         self.authorization = ApiAuthorization(self, self)
-        self.moderation = ApiModerationDeliveryServices(self)
-        self.dicts = Dicts(self, self)
+        self.connections = ApiConnectionDeliveryServices(self)
