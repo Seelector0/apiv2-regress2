45f84f717185e5039af3107efed1e18c032fd5aa
fox140 <andrey.malcev@pimpay.ru>
2023-12-11 17:05:02 +0300

    Comment

--- fixture/database.py
+++ fixture/database.py
@@ -4,6 +4,9 @@ import psycopg2
 
 class DataBase:
 
-    def __init__(self, database):
+    def __init__(self, database: str):
+        r"""Подключение к базе данных.
+        :param database: Название базы данных.
+        """
         self.connection = psycopg2.connect(host=ENV_OBJECT.host(), database=database, user=ENV_OBJECT.db_connections(),
                                            password=ENV_OBJECT.password())
