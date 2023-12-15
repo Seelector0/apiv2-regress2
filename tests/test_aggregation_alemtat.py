5219d6ade3eb02c05b2f0a878b8bb2babae5c76c
fox140 <andrey.malcev@pimpay.ru>
2023-12-15 16:48:45 +0300

    Добавил тесты СД AlemTat

---
+++ tests/test_aggregation_alemtat.py
@@ -0,0 +1,175 @@
+# from utils.global_enums import INFO
+# from utils.checking import Checking
+# from random import choice
+# import pytest
+# import allure
+#
+# Todo добавить после выкатки СД AlemTat.
+# @allure.description("Создание магазина")
+# def test_create_shop(app, connections):
+#     if len(connections.get_list_shops()) == 0:
+#         new_shop = app.shop.post_shop()
+#         Checking.check_status_code(response=new_shop, expected_status_code=201)
+#         Checking.checking_json_key(response=new_shop, expected_value=INFO.created_entity)
+#         Checking.check_value_comparison(
+#             one_value=connections.get_list_shops_value(shop_id=new_shop.json()["id"], value="deleted"),
+#             two_value=[False])
+#         Checking.check_value_comparison(
+#             one_value=connections.get_list_shops_value(shop_id=new_shop.json()["id"], value="visibility"),
+#             two_value=[True])
+#
+#
+# @allure.description("Создание склада")
+# def test_create_warehouse(app, connections):
+#     if len(connections.get_list_warehouses()) == 0:
+#         new_warehouse = app.warehouse.post_warehouse(country_code="KZ")
+#         Checking.check_status_code(response=new_warehouse, expected_status_code=201)
+#         Checking.checking_json_key(response=new_warehouse, expected_value=INFO.created_entity)
+#         Checking.check_value_comparison(
+#             one_value=connections.get_list_warehouses_value(warehouse_id=new_warehouse.json()["id"], value="deleted"),
+#             two_value=[False])
+#         Checking.check_value_comparison(
+#             one_value=connections.get_list_warehouses_value(warehouse_id=new_warehouse.json()["id"],
+#                                                             value="visibility"),
+#             two_value=[True])
+#
+#
+# @allure.description("Подключение настроек СД AlemTat по агрегации")
+# def test_aggregation_delivery_services(app):
+#     boxberry = app.service.post_delivery_services_alemtat()
+#     Checking.check_status_code(response=boxberry, expected_status_code=201)
+#     Checking.checking_json_key(response=boxberry, expected_value=INFO.created_entity)
+#
+#
+# @allure.description("Update Connection Id")
+# def test_update_connection_id(admin):
+#     update = admin.moderation.put_update_connection_id(settings=admin.dicts.form_settings_ds_alemtat())
+#     Checking.check_status_code(response=update, expected_status_code=200)
+#     Checking.checking_json_key(response=update, expected_value=INFO.entity_connections_id)
+#
+#
+# @allure.description("Модерация СД AlemTat")
+# def test_moderation_delivery_services(admin):
+#     moderation = admin.moderation.post_connections_alemtat()
+#     Checking.check_status_code(response=moderation, expected_status_code=200)
+#     Checking.checking_json_key(response=moderation, expected_value=INFO.entity_moderation)
+#
+#
+# @allure.description("Получение Courier оферов по СД AlemTat")
+# @pytest.mark.parametrize("payment_type", ["Paid", "PayOnDelivery"])
+# def test_offers_courier(app, payment_type):
+#     offers_delivery_point = app.offers.get_offers(payment_type=payment_type, types="Courier",
+#                                                   delivery_service_code="AlemTat", country_code="KZ")
+#     Checking.check_status_code(response=offers_delivery_point, expected_status_code=200)
+#     Checking.checking_json_key(response=offers_delivery_point, expected_value=["Courier"])
+#
+#
+# @allure.description("Создание Courier заказа по СД AlemTat")
+# @pytest.mark.parametrize("payment_type", ["Paid", "PayOnDelivery"])
+# def test_create_order_courier(app, payment_type, connections):
+#     new_order = app.order.post_single_order(payment_type=payment_type, type_ds="Courier", service="AlemTat",
+#                                             declared_value=500, country_code="KZ")
+#     Checking.check_status_code(response=new_order, expected_status_code=201)
+#     Checking.checking_json_key(response=new_order, expected_value=INFO.created_entity)
+#     connections.wait_create_order(order_id=new_order.json()["id"])
+#     Checking.check_value_comparison(one_value=connections.get_list_order_value(order_id=new_order.json()["id"],
+#                                                                                value="status"),
+#                                     two_value=["created"])
+#     Checking.check_value_comparison(one_value=connections.get_list_order_value(order_id=new_order.json()["id"],
+#                                                                                value="state"),
+#                                     two_value=["succeeded"])
+#
+#
+# @allure.description("Получение списка заказов CД AlemTat")
+# def test_get_orders(app):
+#     list_orders = app.order.get_orders()
+#     Checking.check_status_code(response=list_orders, expected_status_code=200)
+#     Checking.check_response_is_not_empty(response=list_orders)
+#
+#
+# @allure.description("Получение информации о заказе CД AlemTat")
+# def test_get_order_by_id(app, connections):
+#     random_order = app.order.get_order_id(order_id=choice(connections.get_list_all_orders_out_parcel()))
+#     Checking.check_status_code(response=random_order, expected_status_code=200)
+#     Checking.checking_json_key(response=random_order, expected_value=INFO.entity_order)
+#
+#
+# @allure.description("Получение информации об истории изменения статусов заказа СД AlemTat")
+# def test_order_status(app, connections):
+#     for order_id in connections.get_list_all_orders_out_parcel():
+#         order_status = app.order.get_order_statuses(order_id=order_id)
+#         Checking.check_status_code(response=order_status, expected_status_code=200)
+#         Checking.checking_in_list_json_value(response=order_status, key_name="status", expected_value="created")
+#
+#
+# @allure.description("Получения этикеток CД AlemTat вне партии")
+# def test_get_labels_out_of_parcel(app, connections):
+#     for order_id in connections.get_list_all_orders_out_parcel():
+#         label = app.document.get_label(order_id=order_id)
+#         Checking.check_status_code(response=label, expected_status_code=200)
+#
+#
+# @allure.description("Получение подробной информации о заказе СД AlemTat")
+# def test_order_details(app, connections):
+#     for order_id in connections.get_list_all_orders_out_parcel():
+#         order_details = app.order.get_order_details(order_id=order_id)
+#         Checking.check_status_code(response=order_details, expected_status_code=200)
+#         Checking.checking_json_key(response=order_details, expected_value=INFO.details)
+#
+#
+# @allure.description("Создание партии СД AlemTat")
+# def test_create_parcel(app, connections):
+#     create_parcel = app.parcel.post_parcel(value=choice(connections.get_list_all_orders_out_parcel()))
+#     Checking.check_status_code(response=create_parcel, expected_status_code=207)
+#     Checking.checking_in_list_json_value(response=create_parcel, key_name="type", expected_value="Parcel")
+#
+#
+# @allure.description("Получение списка партий CД AlemTat")
+# def test_get_parcels(app):
+#     list_parcel = app.parcel.get_parcels()
+#     Checking.check_status_code(response=list_parcel, expected_status_code=200)
+#     Checking.check_response_is_not_empty(response=list_parcel)
+#
+#
+# @allure.description("Получение информации о партии CД AlemTat")
+# def test_get_parcel_by_id(app, connections):
+#     random_parcel = app.parcel.get_parcel_id(parcel_id=choice(connections.get_list_parcels()))
+#     Checking.check_status_code(response=random_parcel, expected_status_code=200)
+#     Checking.checking_json_key(response=random_parcel, expected_value=INFO.entity_parcel)
+#
+#
+# @allure.description("Редактирование партии СД AlemTat (Добавление заказов)")
+# def test_add_order_in_parcel(app, connections):
+#     list_parcel_id = connections.get_list_parcels()
+#     for order in connections.get_list_all_orders_out_parcel():
+#         parcel_add = app.parcel.patch_parcel(order_id=order, parcel_id=list_parcel_id[0], op="add")
+#         Checking.check_status_code(response=parcel_add, expected_status_code=200)
+#         assert order in connections.get_list_all_orders_in_parcel()
+#
+#
+# @allure.description("Получение этикетки СД AlemTat")
+# def test_get_label(app, connections):
+#     for order_id in connections.get_list_all_orders_in_parcel():
+#         label = app.document.get_label(order_id=order_id)
+#         Checking.check_status_code(response=label, expected_status_code=200)
+#
+#
+# @allure.description("Получение АПП СД AlemTat")
+# def test_get_app(app):
+#     acceptance = app.document.get_acceptance()
+#     Checking.check_status_code(response=acceptance, expected_status_code=200)
+#
+#
+# @allure.description("Получение документов СД AlemTat")
+# def test_get_documents(app):
+#     documents = app.document.get_files()
+#     Checking.check_status_code(response=documents, expected_status_code=200)
+#
+#
+# @allure.description("Редактирование партии СД AlemTat (Удаление заказа из партии)")
+# def test_remove_order_in_parcel(app, connections):
+#     list_order = connections.get_list_all_orders_in_parcel()
+#     list_parcel_id = connections.get_list_parcels()
+#     remove_order = app.parcel.patch_parcel(op="remove", order_id=choice(list_order), parcel_id=list_parcel_id[0])
+#     Checking.check_status_code(response=remove_order, expected_status_code=200)
+#     assert remove_order is not connections.get_list_all_orders_in_parcel()
