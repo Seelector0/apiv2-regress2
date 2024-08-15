f87d890dcef70de62ce0a2baf3390f60ea4018c6
KirilOdintsov <kirill.odintsov@pimpay.ru>
2024-08-15 14:29:11 +0300

    fix token

--- api/authorization.py
+++ api/authorization.py
@@ -3,7 +3,6 @@ from utils.http_methods import HttpMethod
 from utils.environment import ENV_OBJECT
 import requests
 
-
 class ApiAuthorization:
 
     def __init__(self, app, admin):
@@ -13,11 +12,16 @@ class ApiAuthorization:
         self.response = None
 
     def post_access_token(self, admin: bool = None):
-        r"""Метод получение bearer-токена.
-        :param admin: Параметр для получения bearer-токена для admin api.
+        r"""Метод получения bearer-токена.
+        :param admin: Параметр для получения bearer-токена для admin API.
         """
-        self.response = self.session.post(url=f"{ENV_OBJECT.get_base_url()}/auth/access_token",
-                                          data=Dicts.form_authorization(admin=admin), headers=Dicts.form_headers())
-        if self.response.status_code >= 500:
-            breakpoint()
-        return HttpMethod.return_result(response=self.response)
+        try:
+            self.response = self.session.post(url=f"{ENV_OBJECT.get_base_url()}/auth/access_token",
+                                              data=Dicts.form_authorization(admin=admin), headers=Dicts.form_headers())
+            if self.response.status_code >= 400:
+                print(f"Ошибка при получение токена: {self.response.status_code} - {self.response.text}")
+
+            return HttpMethod.return_result(response=self.response)
+        except requests.RequestException as e:
+            print(f"Ошибка при получение токена: {e}")
+            return None
